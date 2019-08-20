51f2860c496d8d8030fab3e47e527066f0cdd7f2
nreimers <rnils@web.de>
2019-08-20 10:26:19 +0200

    Update version to 0.2.3

--- sentence_transformers/__init__.py
+++ sentence_transformers/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __DOWNLOAD_SERVER__ = 'https://public.ukp.informatik.tu-darmstadt.de/reimers/sentence-transformers/v0.2/'
 from .datasets import SentencesDataset, SentenceLabelDataset
 from .data_samplers import LabelSampler
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", mode="r", encoding="utf-8") as readme_file:
 
 setup(
     name="sentence-transformers",
-    version="0.2.2",
+    version="0.2.3",
     author="Nils Reimers, Gregor Geigle",
     author_email="Rnils@web.de",
     description="Sentence Embeddings using BERT / XLNet",
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     license="Apache License 2.0",
     url="https://github.com/UKPLab/sentence-transformers",
-    download_url="https://github.com/UKPLab/sentence-transformers/archive/v0.2.2.zip",
+    download_url="https://github.com/UKPLab/sentence-transformers/archive/v0.2.3.zip",
     packages=find_packages(),
     install_requires=[
         "pytorch-transformers==1.1.0",
