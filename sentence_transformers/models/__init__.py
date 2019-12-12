5d5310233dbd7312ef21d829c09e424426b44a54
bmaz <beatrice.mazoyer@gmail.com>
2019-12-12 11:56:59 +0100

    add Camembert

--- sentence_transformers/models/__init__.py
+++ sentence_transformers/models/__init__.py
@@ -7,6 +7,7 @@ from .DistilBERT import DistilBERT
 from .LSTM import LSTM
 from .Pooling import Pooling
 from .RoBERTa import RoBERTa
+from .CamemBERT import CamemBERT
 from .WordEmbeddings import WordEmbeddings
 from .WordWeights import WordWeights
 from .XLNet import XLNet
