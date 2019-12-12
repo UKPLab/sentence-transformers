6877aab7f175364604164382264f94d799b60f90
bmaz <beatrice.mazoyer@gmail.com>
2019-12-12 12:19:54 +0100

    Revert "add Camembert"
    This reverts commit 5d5310233dbd7312ef21d829c09e424426b44a54.

--- sentence_transformers/models/__init__.py
+++ sentence_transformers/models/__init__.py
@@ -7,7 +7,6 @@ from .DistilBERT import DistilBERT
 from .LSTM import LSTM
 from .Pooling import Pooling
 from .RoBERTa import RoBERTa
-from .CamemBERT import CamemBERT
 from .WordEmbeddings import WordEmbeddings
 from .WordWeights import WordWeights
 from .XLNet import XLNet
