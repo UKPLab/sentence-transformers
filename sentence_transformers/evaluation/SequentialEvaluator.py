9b94d3fae98970ccdf380139542d93011bb984ea
nreimers <rnils@web.de>
2020-02-03 15:00:43 +0100

    Extending SequentialEvaluator - custom scoring function can be passed

--- sentence_transformers/evaluation/SequentialEvaluator.py
+++ sentence_transformers/evaluation/SequentialEvaluator.py
@@ -6,13 +6,15 @@ class SequentialEvaluator(SentenceEvaluator):
     This evaluator allows that multiple sub-evaluators are passed. When the model is evaluated,
     the data is passed sequentially to all sub-evaluators.
 
-    The score from the last sub-evaluator will be used as the main score for the best model decision.
+    All scores are passed to 'main_score_function', which derives one final score value
     """
-    def __init__(self, evaluators: Iterable[SentenceEvaluator]):
+    def __init__(self, evaluators: Iterable[SentenceEvaluator], main_score_function = lambda scores: scores[-1]):
         self.evaluators = evaluators
+        self.main_score_function = main_score_function
 
     def __call__(self, model, output_path: str = None, epoch: int = -1, steps: int = -1) -> float:
+        scores = []
         for evaluator in self.evaluators:
-            main_score = evaluator(model, output_path, epoch, steps)
+            scores.append(evaluator(model, output_path, epoch, steps))
 
-        return main_score
+        return self.main_score_function(scores)
