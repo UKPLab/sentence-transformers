dfc3b231f89fd6a39ec7f175203acec390b639ac
sean.narenthiran <sean.narenthiran@digitalreasoning.com>
2020-03-30 22:15:04 +0100

    Added initial changes for automodel

--- sentence_transformers/models/BERT.py
+++ sentence_transformers/models/BERT.py
@@ -1,40 +1,47 @@
 from torch import nn
-from transformers import BertModel, BertTokenizer
+from transformers import AutoModel, AutoTokenizer
 import json
 from typing import List
 import os
 import numpy as np
 import logging
 
+
 class BERT(nn.Module):
     """BERT model to generate token embeddings.
 
     Each token is mapped to an output vector from BERT.
     """
+
     def __init__(self, model_name_or_path: str, max_seq_length: int = 128, do_lower_case: bool = True):
         super(BERT, self).__init__()
         self.config_keys = ['max_seq_length', 'do_lower_case']
         self.do_lower_case = do_lower_case
 
-        if max_seq_length > 510:
-            logging.warning("BERT only allows a max_seq_length of 510 (512 with special tokens). Value will be set to 510")
-            max_seq_length = 510
-        self.max_seq_length = max_seq_length
+        self.model = AutoModel.from_pretrained(model_name_or_path)
 
-        self.bert = BertModel.from_pretrained(model_name_or_path)
-        self.tokenizer = BertTokenizer.from_pretrained(model_name_or_path, do_lower_case=do_lower_case)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path,
+                                                       do_lower_case=do_lower_case)
         self.cls_token_id = self.tokenizer.convert_tokens_to_ids([self.tokenizer.cls_token])[0]
         self.sep_token_id = self.tokenizer.convert_tokens_to_ids([self.tokenizer.sep_token])[0]
 
+        if max_seq_length > self.tokenizer.max_len_single_sentence:
+            logging.warning(
+                "BERT only allows a max_seq_length of 510 (512 with special tokens). Value will be set to 510")
+            max_seq_length = self.tokenizer.max_len_single_sentence
+        self.max_seq_length = max_seq_length
+
     def forward(self, features):
         """Returns token_embeddings, cls_token"""
-        output_tokens = self.bert(input_ids=features['input_ids'], token_type_ids=features['token_type_ids'], attention_mask=features['input_mask'])[0]
+        output_tokens = self.model(input_ids=features['input_ids'], token_type_ids=features['token_type_ids'],
+                                   attention_mask=features['input_mask'])[0]
         cls_tokens = output_tokens[:, 0, :]  # CLS token is first token
-        features.update({'token_embeddings': output_tokens, 'cls_token_embeddings': cls_tokens, 'input_mask': features['input_mask']})
+        features.update({'token_embeddings': output_tokens, 'cls_token_embeddings': cls_tokens,
+                         'input_mask': features['input_mask']})
         return features
 
     def get_word_embedding_dimension(self) -> int:
-        return self.bert.config.hidden_size
+        return self.model.config.hidden_size
 
     def tokenize(self, text: str) -> List[int]:
         """
@@ -52,34 +59,64 @@ class BERT(nn.Module):
             the maximal length of the sequence. Cannot be greater than self.sentence_transformer_config.max_seq_length
         :return: embedding ids, segment ids and mask for the sentence
         """
-        pad_seq_length = min(pad_seq_length, self.max_seq_length)
+        max_seq_length = min(pad_seq_length, self.max_seq_length)
 
-        tokens = tokens[:pad_seq_length]
-        input_ids = [self.cls_token_id] + tokens + [self.sep_token_id]
+        sep_token = self.sep_token_id
+        cls_token = self.cls_token_id
+        sequence_a_segment_id = 0
+        cls_token_segment_id = 0  # 2 if using xlnet
+        pad_token_segment_id = 0  # 4 if using xlnet
+        pad_token = 0
+
+        # Account for [CLS] and [SEP] with "- 2" and with "- 3" for RoBERTa.
+        special_tokens_count = self.tokenizer.num_added_tokens()
+
+        tokens = tokens[:(max_seq_length - special_tokens_count)]
+
+        tokens += [sep_token]
+        if sep_token_extra:
+            # roberta uses an extra separator b/w pairs of sentences
+            tokens += [sep_token]
+        token_type_ids = [sequence_a_segment_id] * len(tokens)
+
+        if cls_token_at_end:
+            tokens += [cls_token]
+            token_type_ids += [cls_token_segment_id]
+        else:
+            tokens = [cls_token] + tokens
+
+        input_ids = tokens
         sentence_length = len(input_ids)
-
-        pad_seq_length += 2  ##Add Space for CLS + SEP token
 
         token_type_ids = [0] * len(input_ids)
         input_mask = [1] * len(input_ids)
 
-        # Zero-pad up to the sequence length. BERT: Pad to the right
-        padding = [0] * (pad_seq_length - len(input_ids))
-        input_ids += padding
-        token_type_ids += padding
-        input_mask += padding
+        padding_length = max_seq_length - len(input_ids)
+        if pad_on_left:
+            # Zero-pad up to the sequence length. XLNet: Pad to the left
+            input_ids = ([pad_token] * padding_length) + input_ids
+            input_mask = ([0] * padding_length) + input_mask
+            token_type_ids = ([pad_token_segment_id] * padding_length) + token_type_ids
+        else:
+            # Zero-pad up to the sequence length: Pad to the Right
+            input_ids = input_ids + ([pad_token] * padding_length)
+            input_mask = input_mask + ([0] * padding_length)
+            token_type_ids = token_type_ids + ([pad_token_segment_id] * padding_length)
 
         assert len(input_ids) == pad_seq_length
         assert len(input_mask) == pad_seq_length
         assert len(token_type_ids) == pad_seq_length
 
-        return {'input_ids': np.asarray(input_ids, dtype=np.int64), 'token_type_ids': np.asarray(token_type_ids, dtype=np.int64), 'input_mask': np.asarray(input_mask, dtype=np.int64), 'sentence_lengths': np.asarray(sentence_length, dtype=np.int64)}
+        return {'input_ids': np.asarray(input_ids, dtype=np.int64),
+                'token_type_ids': np.asarray(token_type_ids, dtype=np.int64),
+                'input_mask': np.asarray(input_mask, dtype=np.int64),
+                'sentence_lengths': np.asarray(sentence_length, dtype=np.int64)}
 
     def get_config_dict(self):
         return {key: self.__dict__[key] for key in self.config_keys}
 
     def save(self, output_path: str):
-        self.bert.save_pretrained(output_path)
+        self.model.save_pretrained(output_path)
         self.tokenizer.save_pretrained(output_path)
 
         with open(os.path.join(output_path, 'sentence_bert_config.json'), 'w') as fOut:
@@ -90,9 +127,3 @@ class BERT(nn.Module):
         with open(os.path.join(input_path, 'sentence_bert_config.json')) as fIn:
             config = json.load(fIn)
         return BERT(model_name_or_path=input_path, **config)
-
-
-
-
-
-
