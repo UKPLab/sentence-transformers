dc37af6bef44f1838a87d7630f06b17ca546fde3
sean.narenthiran <sean.narenthiran@digitalreasoning.com>
2020-03-31 10:44:34 +0100

    Added args to change output path location of dataset

--- examples/datasets/get_data.py
+++ examples/datasets/get_data.py
@@ -1,22 +1,25 @@
+import argparse
 import urllib.request
 import zipfile
 import os
-folder_path = os.path.dirname(os.path.realpath(__file__))
-print('Beginning download of datasets')
 
 datasets = ['AllNLI.zip', 'stsbenchmark.zip', 'wikipedia-sections-triplets.zip']
 server = "https://public.ukp.informatik.tu-darmstadt.de/reimers/sentence-transformers/datasets/"
 
-for dataset in datasets:
-    print("Download", dataset)
-    url = server+dataset
-    dataset_path = os.path.join(folder_path, dataset)
-    urllib.request.urlretrieve(url, dataset_path)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_path', default=os.path.dirname(os.path.realpath(__file__)))
+    args = parser.parse_args()
+    print('Beginning download of datasets')
 
-    print("Extract", dataset)
-    with zipfile.ZipFile(dataset_path, "r") as zip_ref:
-        zip_ref.extractall(folder_path)
-    os.remove(dataset_path)
+    for dataset in datasets:
+        print("Downloading", dataset)
+        url = server + dataset
+        dataset_path = os.path.join(args.output_path, dataset)
+        urllib.request.urlretrieve(url, dataset_path)
 
-
-print("All datasets downloaded and extracted")
+        print("Extracting", dataset)
+        with zipfile.ZipFile(dataset_path, "r") as zip_ref:
+            zip_ref.extractall(args.output_path)
+        os.remove(dataset_path)
+    print("All datasets downloaded and extracted")
