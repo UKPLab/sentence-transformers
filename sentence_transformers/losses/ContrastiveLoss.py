005c1b2e5b756dc127a935f21a3f349ed247f34a
chenyunshan <chenyunshan312@163.com>
2020-03-31 23:14:23 +0800

    remove COSINE distance metric in SiameseDistanceMetric class

--- sentence_transformers/losses/ContrastiveLoss.py
+++ sentence_transformers/losses/ContrastiveLoss.py
@@ -11,7 +11,6 @@ class SiameseDistanceMetric(Enum):
     """
     The metric for the contrastive loss
     """
-    COSINE = lambda x, y: 1 - F.cosine_similarity(x, y)
     EUCLIDEAN = lambda x, y: F.pairwise_distance(x, y, p=2)
     MANHATTAN = lambda x, y: F.pairwise_distance(x, y, p=1)
 
