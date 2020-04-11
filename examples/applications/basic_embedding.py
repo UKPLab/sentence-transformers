c68043bac243aa7a6cdf75f3aab446ab35ae2ec8
Nils Reimers <rnils@web.de>
2020-04-11 22:03:59 +0200

    Application examples

---
+++ examples/applications/basic_embedding.py
@@ -0,0 +1,34 @@
+"""
+This basic example loads a pre-trained model from the web and uses it to
+generate sentence embeddings for a given list of sentences.
+"""
+
+from sentence_transformers import SentenceTransformer, LoggingHandler
+import numpy as np
+import logging
+
+#### Just some code to print debug information to stdout
+np.set_printoptions(threshold=100)
+
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+#### /print debug information to stdout
+
+
+
+# Load Sentence model (based on BERT) from URL
+model = SentenceTransformer('bert-base-nli-mean-tokens')
+
+# Embed a list of sentences
+sentences = ['This framework generates embeddings for each input sentence',
+             'Sentences are passed as a list of string.',
+             'The quick brown fox jumps over the lazy dog.']
+sentence_embeddings = model.encode(sentences)
+
+# The result is a list of sentence embeddings as numpy arrays
+for sentence, embedding in zip(sentences, sentence_embeddings):
+    print("Sentence:", sentence)
+    print("Embedding:", embedding)
+    print("")
