38756d9e396c466de2527ad032e1e5cf40eba14d
cpcdoy <cpcdoy@gmail.com>
2020-07-06 17:02:57 +0000

    Add: BatchHardSoftMarginTripletLoss to __init__.py

--- sentence_transformers/losses/__init__.py
+++ sentence_transformers/losses/__init__.py
@@ -1,6 +1,7 @@
 from .CosineSimilarityLoss import *
 from .SoftmaxLoss import *
 from .BatchHardTripletLoss import *
+from .BatchHardSoftMarginTripletLoss import *
 from .MultipleNegativesRankingLoss import *
 from .TripletLoss import *
 from .MSELoss import *
