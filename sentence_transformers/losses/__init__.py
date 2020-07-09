843d37130ae1e0a95cd7af970fc5f81bc20280c4
Nils Reimers <rnils@web.de>
2020-07-09 15:52:48 +0200

    Add ContrastiveLoss

--- sentence_transformers/losses/__init__.py
+++ sentence_transformers/losses/__init__.py
@@ -3,6 +3,7 @@ from .SoftmaxLoss import *
 from .MultipleNegativesRankingLoss import *
 from .TripletLoss import *
 from .MSELoss import *
+from .ContrastiveLoss import *
 # Triplet losses
 from .BatchHardTripletLoss import *
 from .BatchHardSoftMarginTripletLoss import *
