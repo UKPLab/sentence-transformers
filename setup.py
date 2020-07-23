ec5d73bb37358756515efda9dee494b8d16741be
Nils Reimers <rnils@web.de>
2020-07-23 16:50:17 +0200

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", mode="r", encoding="utf-8") as readme_file:
 
 setup(
     name="sentence-transformers",
-    version="0.3.1",
+    version="0.3.2",
     author="Nils Reimers, Gregor Geigle",
     author_email="info@nils-reimers.de",
     description="Sentence Embeddings using BERT / RoBERTa / XLNet",
@@ -15,7 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     license="Apache License 2.0",
     url="https://github.com/UKPLab/sentence-transformers",
-    download_url="https://github.com/UKPLab/sentence-transformers/archive/v0.3.1.zip",
+    download_url="https://github.com/UKPLab/sentence-transformers/archive/v0.3.2.zip",
     packages=find_packages(),
     install_requires=[
         'transformers>=3.0.2',
