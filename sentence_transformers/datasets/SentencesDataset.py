ba63f021fd62fa02a2019604453edc792703a1db
Nils Reimers <rnils@web.de>
2020-08-24 14:39:49 +0200

    Tokenization on the fly

--- sentence_transformers/datasets/SentencesDataset.py
+++ sentence_transformers/datasets/SentencesDataset.py
@@ -1,13 +1,8 @@
 from torch.utils.data import Dataset
 from typing import List
 import torch
-import logging
-from tqdm import tqdm
 from .. import SentenceTransformer
 from ..readers.InputExample import InputExample
-from multiprocessing import Pool, cpu_count
-import multiprocessing
-import time
 
 class SentencesDataset(Dataset):
     """
@@ -18,10 +13,7 @@ class SentencesDataset(Dataset):
     """
     def __init__(self,
                  examples: List[InputExample],
-                 model: SentenceTransformer,
-                 parallel_tokenization: bool = False,
-                 max_processes: int = 4,
-                 chunk_size: int = 5000
+                 model: SentenceTransformer
                  ):
         """
         Create a new SentencesDataset with the tokenized texts and the labels as Tensor
@@ -30,95 +22,19 @@ class SentencesDataset(Dataset):
             A list of sentence.transformers.readers.InputExample
         :param model:
             SentenceTransformerModel
-        :param parallel_tokenization
-            If true, multiple processes will be started for the tokenization
-        :param max_processes
-            Maximum number of processes started for tokenization. Cannot be larger can cpu_count()
-        :param chunk_size
-            #chunk_size number of examples are send to each process. Larger values increase overall tokenization speed
         """
         self.model = model
-        self.max_processes = min(max_processes, cpu_count())
-        self.chunk_size = chunk_size
-        self.tokens = None
-        self.labels = None
-        self.parallel_tokenization = parallel_tokenization
-
-        if self.parallel_tokenization:
-            if multiprocessing.get_start_method() != 'fork':
-                logging.info("Parallel tokenization is only available on Unix systems which allow to fork processes. Fall back to sequential tokenization")
-                self.parallel_tokenization = False
-
         self.examples = examples
-        self.convert_input_examples()
-
-    def convert_input_examples(self):
-        """
-        Converts input examples to a SmartBatchingDataset usable to train the model with
-        SentenceTransformer.smart_batching_collate as the collate_fn for the DataLoader
-
-        smart_batching_collate as collate_fn is required because it transforms the tokenized texts to the tensors.
-
-        :param examples:
-            the input examples for the training
-        :param model
-            the Sentence BERT model for the conversion
-
-        """
-        num_texts = len(self.examples[0].texts)
-        inputs = [[] for _ in range(num_texts)]
-        labels = []
-        too_long = [0] * num_texts
-        label_type = None
-
-        if isinstance(self.examples[0].label, int):
-            label_type = torch.long
-        elif isinstance(self.examples[0].label, float):
-            label_type = torch.float
-
-        max_seq_length = self.model.get_max_seq_length()
-
-        logging.info("Start tokenization")
-        if not self.parallel_tokenization or self.max_processes == 1 or len(self.examples) <= self.chunk_size:
-            tokenized_texts = [self.tokenize_example(example) for example in tqdm(self.examples)]
-        else:
-            logging.info("Use multi-process tokenization with {} processes".format(self.max_processes))
-            self.model.to('cpu')
-            with Pool(self.max_processes) as p:
-                tokenized_texts = list(tqdm(p.imap(self.tokenize_example, self.examples, chunksize=self.chunk_size), total=len(self.examples), smoothing=0))
-
-        for ex_index, example in enumerate(self.examples):
-            example.texts_tokenized = tokenized_texts[ex_index]
-
-            for i, token in enumerate(example.texts_tokenized):
-                if max_seq_length is not None and max_seq_length > 0 and len(token) >= max_seq_length:
-                    too_long[i] += 1
-
-            labels.append(example.label if example.label is not None else 0)
-
-            for i in range(num_texts):
-                inputs[i].append(example.texts_tokenized[i])
-
-        labels = torch.tensor(labels, dtype=label_type)
-
-        logging.info("Num sentences: %d" % (len(self.examples)))
-        for i in range(num_texts):
-            logging.info("Sentences {} longer than max_seqence_length: {}".format(i, too_long[i]))
-
-        self.tokens = inputs
-        self.labels = labels
-
-
-    def tokenize_example(self, example):
-        if example.texts_tokenized is not None:
-            return example.texts_tokenized
-
-        return [self.model.tokenize(text) for text in example.texts]
+        self.label_type = torch.long if isinstance(self.examples[0].label, int) else torch.float
 
 
     def __getitem__(self, item):
-        return [self.tokens[i][item] for i in range(len(self.tokens))], self.labels[item]
+        label = torch.tensor(self.examples[item].label, dtype=self.label_type)
+        if self.examples[item].texts_tokenized is None:
+            self.examples[item].texts_tokenized = [self.model.tokenize(text) for text in self.examples[item].texts]
+
+        return self.examples[item].texts_tokenized, label
 
 
     def __len__(self):
-        return len(self.tokens[0])
+        return len(self.examples)
