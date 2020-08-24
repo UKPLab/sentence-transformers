fc0ac5f4151b82984c136db9d7b2dfbd5509ea9c
Nils Reimers <rnils@web.de>
2020-08-24 17:55:16 +0200

    update version

--- sentence_transformers/__init__.py
+++ sentence_transformers/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __DOWNLOAD_SERVER__ = 'https://public.ukp.informatik.tu-darmstadt.de/reimers/sentence-transformers/v0.2/'
 from .datasets import SentencesDataset, SentenceLabelDataset, ParallelSentencesDataset
 from .LoggingHandler import LoggingHandler
