cddb49430687256aab82607e00b5a4028d141be9
Nils Reimers <rnils@web.de>
2020-08-27 14:52:58 +0200

    Improved performance for transitive closure

--- sentence_transformers/evaluation/ParaphraseMiningEvaluator.py
+++ sentence_transformers/evaluation/ParaphraseMiningEvaluator.py
@@ -55,16 +55,7 @@ class ParaphraseMiningEvaluator(SentenceEvaluator):
 
         #Add transitive closure
         if add_transitive_closure:
-            new_entries = True
-            while new_entries:
-                new_entries = False
-                for a in self.duplicates:
-                    for b in list(self.duplicates[a]):
-                        for c in list(self.duplicates[b]):
-                            if a != c and not self.duplicates[a][c]:
-                                new_entries = True
-                                self.duplicates[a][c] = True
-                                self.duplicates[c][a] = True
+            self.duplicates = self.add_transitive_closure(self.duplicates)
 
 
         positive_key_pairs = set()
@@ -142,5 +133,33 @@ class ParaphraseMiningEvaluator(SentenceEvaluator):
                     writer.writerow([epoch, steps, best_precision, best_recall, best_f1, threshold, average_precision])
 
         return average_precision
+
+
+    @staticmethod
+    def add_transitive_closure(graph):
+        nodes_visited = set()
+        for a in list(graph.keys()):
+            if a not in nodes_visited:
+                connected_subgraph_nodes = set()
+                connected_subgraph_nodes.add(a)
+
+                # Add all nodes in the connected graph
+                neighbor_nodes_queue = list(graph[a])
+                while len(neighbor_nodes_queue) > 0:
+                    node = neighbor_nodes_queue.pop(0)
+                    if node not in connected_subgraph_nodes:
+                        connected_subgraph_nodes.add(node)
+                        neighbor_nodes_queue.extend(graph[node])
+
+                # Ensure transitivity between all nodes in the graph
+                connected_subgraph_nodes = list(connected_subgraph_nodes)
+                for i in range(len(connected_subgraph_nodes) - 1):
+                    for j in range(i + 1, len(connected_subgraph_nodes)):
+                        graph[connected_subgraph_nodes[i]][connected_subgraph_nodes[j]] = True
+                        graph[connected_subgraph_nodes[j]][connected_subgraph_nodes[i]] = True
+
+                        nodes_visited.add(connected_subgraph_nodes[i])
+                        nodes_visited.add(connected_subgraph_nodes[j])
+        return graph
 
 
