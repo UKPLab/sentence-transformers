a3b6e401fbb348f1937516cf58c38bee0eeaa3dd
Nils Reimers <rnils@web.de>
2020-09-04 10:31:48 +0200

    Update training_wikipedia_sections.py example

--- examples/training/other/training_wikipedia_sections.py
+++ examples/training/other/training_wikipedia_sections.py
@@ -8,15 +8,15 @@ See docs/pretrained-models/wikipedia-sections-modesl.md for further details.
 You can get the dataset by running examples/datasets/get_data.py
 """
 
-from sentence_transformers import SentenceTransformer, SentencesDataset, LoggingHandler, losses, models
+from sentence_transformers import SentenceTransformer, SentencesDataset, InputExample, LoggingHandler, losses, models, util
 from torch.utils.data import DataLoader
-from sentence_transformers.readers import TripletReader
 from sentence_transformers.evaluation import TripletEvaluator
 from datetime import datetime
+from zipfile import ZipFile
 
 import csv
 import logging
-
+import os
 
 
 logging.basicConfig(format='%(asctime)s - %(message)s',
@@ -26,12 +26,19 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 
 
 #You can specify any huggingface/transformers pre-trained model here, for example, bert-base-uncased, roberta-base, xlm-roberta-base
-model_name = 'bert-base-uncased'
+model_name = 'distilbert-base-uncased'
+
+dataset_path = 'datasets/wikipedia-sections'
+if not os.path.exists(dataset_path):
+    os.makedirs(dataset_path, exist_ok=True)
+    filepath = os.path.join(dataset_path, 'wikipedia-sections-triplets.zip')
+    util.http_get('https://sbert.net/datasets/wikipedia-sections-triplets.zip', filepath)
+    with ZipFile(filepath, 'r') as zip:
+        zip.extractall(dataset_path)
 
 
 ### Create a torch.DataLoader that passes training batch instances to our model
 train_batch_size = 16
-triplet_reader = TripletReader('../datasets/wikipedia-sections-triplets', s1_col_idx=1, s2_col_idx=2, s3_col_idx=3, delimiter=',', quoting=csv.QUOTE_MINIMAL, has_header=True)
 output_path = "output/training-wikipedia-sections-"+model_name+"-"+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 num_epochs = 1
 
@@ -50,12 +57,28 @@ model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
 
 logging.info("Read Triplet train dataset")
-train_dataset = SentencesDataset(examples=triplet_reader.get_examples('train.csv', max_examples=100000), model=model)
+train_examples = []
+with open(os.path.join(dataset_path, 'train.csv'), encoding="utf-8") as fIn:
+    reader = csv.DictReader(fIn, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    for row in reader:
+        train_examples.append(InputExample(texts=[row['Sentence1'], row['Sentence2'], row['Sentence3']], label=0))
+
+
+train_dataset = SentencesDataset(train_examples, model=model)
 train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=train_batch_size)
 train_loss = losses.TripletLoss(model=model)
 
 logging.info("Read Wikipedia Triplet dev dataset")
-evaluator = TripletEvaluator.from_input_examples(triplet_reader.get_examples('validation.csv', 1000), name='dev')
+dev_examples = []
+with open(os.path.join(dataset_path, 'validation.csv'), encoding="utf-8") as fIn:
+    reader = csv.DictReader(fIn, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    for row in reader:
+        dev_examples.append(InputExample(texts=[row['Sentence1'], row['Sentence2'], row['Sentence3']]))
+
+        if len(dev_examples) >= 1000:
+            break
+
+evaluator = TripletEvaluator.from_input_examples(dev_examples, name='dev')
 
 
 warmup_steps = int(len(train_dataset) * num_epochs / train_batch_size * 0.1) #10% of train data
@@ -75,7 +98,15 @@ model.fit(train_objectives=[(train_dataloader, train_loss)],
 #
 ##############################################################################
 
+logging.info("Read test examples")
+test_examples = []
+with open(os.path.join(dataset_path, 'test.csv'), encoding="utf-8") as fIn:
+    reader = csv.DictReader(fIn, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    for row in reader:
+        test_examples.append(InputExample(texts=[row['Sentence1'], row['Sentence2'], row['Sentence3']]))
+
+
 model = SentenceTransformer(output_path)
-test_evaluator = TripletEvaluator.from_input_examples(triplet_reader.get_examples('test.csv'), name='test')
+test_evaluator = TripletEvaluator.from_input_examples(test_examples, name='test')
 test_evaluator(model, output_path=output_path)
 
