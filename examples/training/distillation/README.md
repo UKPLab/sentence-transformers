54fa547aa130fa46968ed064f22a79a6ecae075f
Nils Reimers <rnils@web.de>
2020-09-28 14:27:06 +0200

    Add docs to model distillation

--- examples/training/distillation/README.md
+++ examples/training/distillation/README.md
@@ -1,5 +1,34 @@
 # Model Distillation - Faster, cheaper & lighter Model
-This folder contains example to make SentenceTransformer models faster, cheaper and lighter.
+This folder contains example to make SentenceTransformer models faster, cheaper and lighter. These light models achieve 97.5% - 100% performance of the original model on downstream tasks.
+
+# Knowledge Distillation
+Knowledge distillation describes the process to transfer knowledge from a  teacher model to a student model. It can be used to extend sentence embeddings to new languages ([Making Monolingual Sentence Embeddings Multilingual using Knowledge Distillation](https://arxiv.org/abs/2004.09813)), but the traditional approach is to have slow (but well performing) teacher model and a fast student model.
+
+The fast student model imitates the teacher model and achieves by this a high performance. 
+
+![Knowledge Distillation](https://raw.githubusercontent.com/UKPLab/sentence-transformers/master/docs/img/monolingual-distillation.png)
+
+
+We implement two options for creating the student model:
+1) Use a light transformer model like TinyBERT or BERT-Small to imitate the teacher.
+2) We take the teacher model and keep only certain layers, for example, only 4 layers.
+
+Option 2) works usually better, as we keep most of the weights from the teacher. In Option 1, we have to tune all
+weights in the student from scratch.
+
+## Speed - Performance Trade-Off
+Smaller models are faster, but show a (slightly) worse performance when evaluated on down stream tasks. To get an impression of this trade-off, we show some numbers of the *roberta-base-nli-stsb-mean-tokens* model with different number of layers:
+
+| Layers | STSbenchmark Performance | Performance Decrease |Speed (Sent. / Sec. on V100-GPU) |
+| ---- |: ---- :|: ---- :|: ---- :|
+| teacher: 12 | 85.44 | - | 2300 |
+| 8 | 85.54 | +0.1% | 3200 |
+| 6 | 85.23 | -0.2% | 4000 |
+| 4 | 84.92 | -0.6% | 5300 |
+| 3 | | | 6500 |
+| 2 | 83.32 | -2.5% | 7700 |
+| 1 | | | 9200 |
+
 
 ## Dimension Reduction
 By default, the pretrained models output embeddings with size 768 (base-models) or with size 1024 (large-models). However, when you store Millions of embeddings, this can require quite a lot of memory / storage.
@@ -9,4 +38,5 @@ By default, the pretrained models output embeddings with size 768 (base-models) 
 This dimensionality reduction technique can easily be applied to existent models. We could even reduce the embeddings size to 32, reducing the storage requirment by factor 24 (performance decreases to 81.82). 
 
 Note: This technique neither improves the runtime, nor the memory requirement for running the model. It only reduces the needed space to store embeddings, for example, for [semantic search](https://www.sbert.net/docs/usage/semantic_search.html).
+
 
--- examples/training/distillation/model_distillation.py
+++ examples/training/distillation/model_distillation.py
@@ -1,6 +1,28 @@
+"""
+This file contains an example how to make a SentenceTransformer model faster and lighter.
+
+This is achieved by using Knowledge Distillation: We use a well working teacher model to train
+a fast and light student model. The student model learns to imitate the produced
+sentence embeddings from the teacher. We train this on a diverse set of sentences we got
+from SNLI + Multi+NLI + Wikipedia.
+
+After the distillation is finished, the student model produce nearly the same embeddings as the
+teacher, however, it will be much faster.
+
+The script implements to options two options to initialize the student:
+Option 1: Train a light transformer model like TinyBERT to imitate the teacher
+Option 2: We take the teacher model and keep only certain layers, for example, only 4 layers.
+
+Option 2) works usually better, as we keep most of the weights from the teacher. In Option 1, we have to tune all
+weights in the student from scratch.
+
+There is a performance - speed trade-off. However, we found that a student with 4 instead of 12 layers keeps about 99.4%
+of the teacher performance, while being 2.3 times faster.
+"""
 from torch.utils.data import DataLoader
 from sentence_transformers import models, losses, evaluation
 from sentence_transformers import LoggingHandler, SentenceTransformer, util, InputExample
+from sentence_transformers.datasets import ParallelSentencesDataset
 import logging
 from datetime import datetime
 import os
@@ -9,7 +31,7 @@ import csv
 import random
 from sklearn.decomposition import PCA
 import torch
-from sentence_transformers.datasets import ParallelSentencesDataset
+
 
 #### Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
@@ -23,6 +45,9 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 teacher_model_name = 'roberta-base-nli-stsb-mean-tokens'
 teacher_model = SentenceTransformer(teacher_model_name)
 
+output_path = "output/model-distillation-3layers-" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+
 use_layer_reduction = True
 
 #There are two options to create a light and fast student model:
@@ -32,12 +57,18 @@ if use_layer_reduction:
 
     # Get the transformer model
     auto_model = student_model._first_module().auto_model
-    auto_model_encoder = auto_model.encoder
 
-    # Which layers to keep from the teacher model
-    layers_to_keep = [0, 3, 6, 9]
-    new_layers = torch.nn.ModuleList([layer_module for i, layer_module in enumerate(auto_model_encoder.layer) if i in layers_to_keep])
-    auto_model_encoder.layer = new_layers
+    # Which layers to keep from the teacher model. We equally spread the layers to keep over the original teacher
+    #layers_to_keep = [5]
+    #layers_to_keep = [3, 7]
+    #layers_to_keep = [3, 7, 11]
+    layers_to_keep = [1, 4, 7, 10]          #Keep 4 layers from the teacher
+    #layers_to_keep = [0, 2, 4, 6, 8, 10]
+    #layers_to_keep = [0, 1, 3, 4, 6, 7, 9, 10]
+
+    logging.info("Remove layers from teacher. Only keep these layer: {}".format(layers_to_keep))
+    new_layers = torch.nn.ModuleList([layer_module for i, layer_module in enumerate(auto_model.encoder.layer) if i in layers_to_keep])
+    auto_model.encoder.layer = new_layers
     auto_model.config.num_hidden_layers = len(layers_to_keep)
 else:
     # 2) The other option is to train a small model like TinyBERT to imitate the teacher.
@@ -51,10 +82,13 @@ else:
 inference_batch_size = 64
 train_batch_size = 64
 
-output_path = "output/model-distillation-" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
 
 #We use AllNLI as a source of sentences for the distillation
 nli_dataset_path = 'datasets/AllNLI.tsv.gz'
+
+#Further, we use sentences extracted from the English Wikipedia to train the distillation
+wikipedia_dataset_path = 'datasets/wikipedia-en-sentences.txt.gz'
 
 #We use the STS benchmark dataset to see how much performance we loose
 sts_dataset_path = 'datasets/stsbenchmark.tsv.gz'
@@ -64,29 +98,43 @@ sts_dataset_path = 'datasets/stsbenchmark.tsv.gz'
 if not os.path.exists(nli_dataset_path):
     util.http_get('https://sbert.net/datasets/AllNLI.tsv.gz', nli_dataset_path)
 
+if not os.path.exists(wikipedia_dataset_path):
+    util.http_get('https://sbert.net/datasets/wikipedia-en-sentences.txt.gz', wikipedia_dataset_path)
+
 if not os.path.exists(sts_dataset_path):
     util.http_get('https://sbert.net/datasets/stsbenchmark.tsv.gz', sts_dataset_path)
 
-#Read ALLNLI dataset. We use this dataset, to tune the student model to imitate the teacher embeddings
-nli_sentences_train = set()
-nli_sentences_dev = set()
+#We need sentences to train our distillation. Here, we use sentences from AllNLI and from WikiPedia
+train_sentences_nli = set()
+dev_sentences_nli = set()
 
+train_sentences_wikipedia = []
+dev_sentences_wikipedia = []
+
+# Read ALLNLI
 with gzip.open(nli_dataset_path, 'rt', encoding='utf8') as fIn:
     reader = csv.DictReader(fIn, delimiter='\t', quoting=csv.QUOTE_NONE)
     for row in reader:
         if row['split'] == 'dev':
-            nli_sentences_dev.add(row['sentence1'])
-            nli_sentences_dev.add(row['sentence2'])
+            dev_sentences_nli.add(row['sentence1'])
+            dev_sentences_nli.add(row['sentence2'])
         else:
-            nli_sentences_train.add(row['sentence1'])
-            nli_sentences_train.add(row['sentence2'])
+            train_sentences_nli.add(row['sentence1'])
+            train_sentences_nli.add(row['sentence2'])
 
-nli_sentences_train = list(nli_sentences_train)
-random.shuffle(nli_sentences_train)
+train_sentences_nli = list(train_sentences_nli)
+random.shuffle(train_sentences_nli)
 
-nli_sentences_dev = list(nli_sentences_dev)
-random.shuffle(nli_sentences_dev)
-nli_sentences_dev = nli_sentences_dev[0:10000] #Limit dev sentences to 10k
+dev_sentences_nli = list(dev_sentences_nli)
+random.shuffle(dev_sentences_nli)
+dev_sentences_nli = dev_sentences_nli[0:5000] #Limit dev sentences to 5k
+
+# Read Wikipedia sentences file
+with gzip.open(wikipedia_dataset_path, 'rt', encoding='utf8') as fIn:
+    wikipeda_sentences = [line.strip() for line in fIn]
+
+dev_sentences_wikipedia = wikipeda_sentences[0:5000] #Use the first 5k sentences from the wikipedia file for development
+train_sentences_wikipedia = wikipeda_sentences[5000:]
 
 
 # We use the STS benchmark dataset to measure the performance of student model im comparison to the teacher model
@@ -95,7 +143,7 @@ dev_samples = []
 with gzip.open(sts_dataset_path, 'rt', encoding='utf8') as fIn:
     reader = csv.DictReader(fIn, delimiter='\t', quoting=csv.QUOTE_NONE)
     for row in reader:
-        if row['split'] == 'test':
+        if row['split'] == 'dev':
             score = float(row['score']) / 5.0 #Normalize score to range 0 ... 1
             dev_samples.append(InputExample(texts=[row['sentence1'], row['sentence2']], label=score))
 
@@ -108,7 +156,7 @@ dev_evaluator_sts(teacher_model)
 # Student Model has fewer dimension. Compute PCA for the teacher to reduce the dimensions
 if student_model.get_sentence_embedding_dimension() < teacher_model.get_sentence_embedding_dimension():
     logging.info("Student model has fewer dimensions that the teacher. Compute PCA for down projection")
-    pca_sentences = nli_sentences_train[0:25000]
+    pca_sentences = train_sentences_nli[0:25000]
     pca_embeddings = teacher_model.encode(pca_sentences, convert_to_numpy=True)
     pca = PCA(n_components=student_model.get_sentence_embedding_dimension())
     pca.fit(pca_embeddings)
@@ -127,13 +175,15 @@ if student_model.get_sentence_embedding_dimension() < teacher_model.get_sentence
 # For this, we need a large set of sentences. These sentences are embedded using the teacher model,
 # and the student tries to mimic these embeddings. It is the same approach as used in: https://arxiv.org/abs/2004.09813
 train_data = ParallelSentencesDataset(student_model=student_model, teacher_model=teacher_model, batch_size=inference_batch_size, use_embedding_cache=False)
-train_data.add_dataset([[sent] for sent in nli_sentences_train])
+train_data.add_dataset([[sent] for sent in train_sentences_nli], max_sentence_length=256)
+train_data.add_dataset([[sent] for sent in train_sentences_wikipedia], max_sentence_length=256)
 
 train_dataloader = DataLoader(train_data, shuffle=True, batch_size=train_batch_size)
 train_loss = losses.MSELoss(model=student_model)
 
 # We create an evaluator, that measure the Mean Squared Error (MSE) between the teacher and the student embeddings
-dev_evaluator_mse = evaluation.MSEEvaluator(nli_sentences_dev, nli_sentences_dev, teacher_model=teacher_model)
+dev_sentences = dev_sentences_nli + dev_sentences_wikipedia
+dev_evaluator_mse = evaluation.MSEEvaluator(dev_sentences, dev_sentences, teacher_model=teacher_model)
 
 # Train the student model to imitate the teacher
 student_model.fit(train_objectives=[(train_dataloader, train_loss)],
