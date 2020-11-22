e0aa596a0397a41ba69f75c1124318f0cb1dceca
Nils Reimers <rnils@web.de>
2020-11-22 13:52:43 +0100

    New setup for cross encoder training MS Marco

--- examples/training/ms_marco/train_cross-encoder.py
+++ examples/training/ms_marco/train_cross-encoder.py
@@ -16,7 +16,7 @@ python train_cross-encoder.py
 from torch.utils.data import DataLoader
 from sentence_transformers import LoggingHandler, util
 from sentence_transformers.cross_encoder import CrossEncoder
-from sentence_transformers.cross_encoder.evaluation import CEBinaryClassificationEvaluator
+from sentence_transformers.cross_encoder.evaluation import CERerankingEvaluator
 from sentence_transformers import InputExample
 import logging
 from datetime import datetime
@@ -24,6 +24,7 @@ import gzip
 import sys
 import os
 import tarfile
+import tqdm
 
 #### Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
@@ -34,7 +35,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 
 
 #First, we define the transformer model we want to fine-tune
-model_name = 'google/electra-small-discriminator'
+model_name = 'distilroberta-base'
 train_batch_size = 32
 num_epochs = 1
 model_save_path = 'output/training_ms-marco_cross-encoder-'+model_name.replace("/", "-")+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -47,6 +48,9 @@ model_save_path = 'output/training_ms-marco_cross-encoder-'+model_name.replace("
 # specify (query, positive sample, negative sample).
 pos_neg_ration = 4
 
+# Maximal number of training samples we want to use
+max_train_samples = 2e7
+
 #We set num_labels=1, which predicts a continous score between 0 and 1
 model = CrossEncoder(model_name, num_labels=1, max_length=512)
 
@@ -54,8 +58,6 @@ model = CrossEncoder(model_name, num_labels=1, max_length=512)
 ### Now we read the MS Marco dataset
 data_folder = 'msmarco-data'
 os.makedirs(data_folder, exist_ok=True)
-
-
 
 
 #### Read the corpus files, that contain all the passages. Store them in the corpus dict
@@ -97,25 +99,51 @@ with open(queries_filepath, 'r', encoding='utf8') as fIn:
 
 
 ### Now we create our training & dev data
-### As dev data, we use the first 10k samples from the training corpus
-num_dev_samples = 10000
+train_samples = []
+dev_samples = {}
+
+# We use 200 random queries from the train set for evaluation during training
+# Each query has at least one relevant and up to 200 irrelevant (negative) passages
+num_dev_queries = 200
+num_max_dev_negatives = 200
+
+# msmarco-qidpidtriples.rnd-shuf.train-eval.tsv.gz and msmarco-qidpidtriples.rnd-shuf.train.tsv.gz is a randomly
+# shuffled version of qidpidtriples.train.full.2.tsv.gz from the MS Marco website
+# We extracted in the train-eval split 500 random queries that can be used for evaluation during training
+train_eval_filepath = os.path.join(data_folder, 'msmarco-qidpidtriples.rnd-shuf.train-eval.tsv.gz')
+if not os.path.exists(train_eval_filepath):
+    logging.info("Download "+os.path.basename(train_eval_filepath))
+    util.http_get('https://sbert.net/datasets/msmarco-qidpidtriples.rnd-shuf.train-eval.tsv.gz', train_eval_filepath)
+
+with gzip.open(train_eval_filepath, 'rt') as fIn:
+    for line in fIn:
+        qid, pos_id, neg_id = line.strip().split()
+
+        if qid not in dev_samples and len(dev_samples) < num_dev_queries:
+            dev_samples[qid] = {'query': queries[qid], 'positive': set(), 'negative': set()}
+
+        if qid in dev_samples:
+            dev_samples[qid]['positive'].add(corpus[pos_id])
+
+            if len(dev_samples[qid]['negative']) < num_max_dev_negatives:
+                dev_samples[qid]['negative'].add(corpus[neg_id])
+
+
+# Read our training file
+train_filepath = os.path.join(data_folder, 'msmarco-qidpidtriples.rnd-shuf.train.tsv.gz')
+if not os.path.exists(train_filepath):
+    logging.info("Download "+os.path.basename(train_filepath))
+    util.http_get('https://sbert.net/datasets/msmarco-qidpidtriples.rnd-shuf.train.tsv.gz', train_filepath)
 
 cnt = 0
-train_samples = []
-dev_samples = []
-dev_qids = set()    #Stores which query ids (qids) have been included in dev_samples. So that we don't use them in the training set
-
-triplets_filepath = os.path.join(data_folder, 'qidpidtriples.train.full.2.tsv.gz')
-if not os.path.exists(triplets_filepath):
-    util.http_get('https://msmarco.blob.core.windows.net/msmarcoranking/qidpidtriples.train.full.2.tsv.gz', triplets_filepath)
-
-with gzip.open(triplets_filepath, 'rt') as fIn:
-    for line in fIn:
-        cnt += 1
+with gzip.open(train_filepath, 'rt') as fIn:
+    for line in tqdm.tqdm(fIn, unit_scale=True):
         qid, pos_id, neg_id = line.strip().split()
-        query = queries[qid]
 
-        #Select alternatively positive and negative samples according to our positive-to-negative ratio.
+        if qid in dev_samples:
+            continue
+
+        query = queries[qid]
         if (cnt % (pos_neg_ration+1)) == 0:
             passage = corpus[pos_id]
             label = 1
@@ -123,19 +151,18 @@ with gzip.open(triplets_filepath, 'rt') as fIn:
             passage = corpus[neg_id]
             label = 0
 
-        example = InputExample(texts=[query, passage], label=label)
-        if len(dev_samples) <= num_dev_samples:
-            dev_samples.append(example)
-            dev_qids.add(qid)
-        elif qid not in dev_qids:
-            train_samples.append(example)
+        train_samples.append(InputExample(texts=[query, passage], label=label))
+        cnt += 1
+
+        if cnt >= max_train_samples:
+            break
 
 # We create a DataLoader to load our train samples
 train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=train_batch_size)
 
 # We add an evaluator, which evaluates the performance during training
 # It performs a classification task and measures scores like F1 (finding relevant passages) and Average Precision
-evaluator = CEBinaryClassificationEvaluator.from_input_examples(dev_samples, name='MS-Marco')
+evaluator = CERerankingEvaluator(dev_samples, name='train-eval')
 
 # Configure the training
 warmup_steps = 5000
@@ -146,7 +173,7 @@ logging.info("Warmup-steps: {}".format(warmup_steps))
 model.fit(train_dataloader=train_dataloader,
           evaluator=evaluator,
           epochs=num_epochs,
-          evaluation_steps=5000,
+          evaluation_steps=10000,
           warmup_steps=warmup_steps,
           output_path=model_save_path,
           use_amp=True)
