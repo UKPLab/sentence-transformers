813a066d22127090c0c78f531efad519ea849961
Jose Marcos Rodríguez Fernández <jose.marcos.rf@gmail.com>
2020-12-13 23:34:28 +0100

    init logger. WIP

--- sentence_transformers/__init__.py
+++ sentence_transformers/__init__.py
@@ -1,8 +1,16 @@
 __version__ = "0.3.9"
+
 __DOWNLOAD_SERVER__ = 'https://sbert.net/models/'
+
+import logging
+
 from .datasets import SentencesDataset, SentenceLabelDataset, ParallelSentencesDataset
 from .LoggingHandler import LoggingHandler
 from .SentenceTransformer import SentenceTransformer
 from .readers import InputExample
 from .cross_encoder.CrossEncoder import CrossEncoder
+
+
+logger = logging.getLogger(__name__)
+
 
