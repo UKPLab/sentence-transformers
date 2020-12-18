f27fb5f544c98ab7d34d38fc475c882973895520
Nils Reimers <rnils@web.de>
2020-12-18 11:18:22 +0100

    replace devision by torch.nn.functional.normalize function

--- sentence_transformers/util.py
+++ sentence_transformers/util.py
@@ -28,8 +28,8 @@ def pytorch_cos_sim(a: Tensor, b: Tensor):
     if len(b.shape) == 1:
         b = b.unsqueeze(0)
 
-    a_norm = a / a.norm(dim=1)[:, None]
-    b_norm = b / b.norm(dim=1)[:, None]
+    a_norm = torch.nn.functional.normalize(a, p=2, dim=1)
+    b_norm = torch.nn.functional.normalize(b, p=2, dim=1)
     return torch.mm(a_norm, b_norm.transpose(0, 1))
 
 
