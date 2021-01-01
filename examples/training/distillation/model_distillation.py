f29c4eb154748830269f1ccfd332bf39d630087e
Nils Reimers <rnils@web.de>
2021-01-01 21:46:54 +0100

    update model name

--- examples/training/distillation/model_distillation.py
+++ examples/training/distillation/model_distillation.py
@@ -42,7 +42,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 
 
 # Teacher Model: Model we want to distill to a smaller model
-teacher_model_name = 'roberta-base-nli-stsb-mean-tokens'
+teacher_model_name = 'stsb-roberta-base'
 teacher_model = SentenceTransformer(teacher_model_name)
 
 output_path = "output/model-distillation-" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -66,7 +66,7 @@ if use_layer_reduction:
     #layers_to_keep = [0, 2, 4, 6, 8, 10]
     #layers_to_keep = [0, 1, 3, 4, 6, 7, 9, 10]
 
-    logging.info("Remove layers from teacher. Only keep these layers: {}".format(layers_to_keep))
+    logging.info("Remove layers from student. Only keep these layers: {}".format(layers_to_keep))
     new_layers = torch.nn.ModuleList([layer_module for i, layer_module in enumerate(auto_model.encoder.layer) if i in layers_to_keep])
     auto_model.encoder.layer = new_layers
     auto_model.config.num_hidden_layers = len(layers_to_keep)
