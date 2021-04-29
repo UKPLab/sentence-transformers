022b2ddb790a45be821066f7ff35f4b375a6cd97
Nils Reimers <rnils@web.de>
2021-04-29 09:17:46 +0200

    bugfix TSDAE

--- sentence_transformers/losses/DenoisingAutoEncoderLoss.py
+++ sentence_transformers/losses/DenoisingAutoEncoderLoss.py
@@ -3,8 +3,9 @@ from torch import nn, Tensor
 from typing import Iterable, Dict
 from sentence_transformers import SentenceTransformer
 from transformers import AutoConfig, AutoTokenizer, AutoModelForCausalLM, PreTrainedModel
-import copy
+import logging
 
+logger = logging.getLogger(__name__)
 
 class DenoisingAutoEncoderLoss(nn.Module):
     """
@@ -13,17 +14,17 @@ class DenoisingAutoEncoderLoss(nn.Module):
         During training, the decoder reconstructs the original sentences from the encoded sentence embeddings.
         Here the argument 'decoder_name_or_path' indicates the pretrained model (supported by Huggingface) to be used as the decoder.
         Since decoding process is included, here the decoder should have a class called XXXLMHead (in the context of Huggingface's Transformers).
-        One can also refer to examples/unsupervised_learning/denoising_autoencoder/modeling_distilbert.py as an example for manually supporting the LMHead class.
-        Flag 'tie_encoder_decoder' indicates whether to tie the trainable parameters of encoder and decoder, 
+        Flag 'tie_encoder_decoder' indicates whether to tie the trainable parameters of encoder and decoder,
         which is shown beneficial to model performance while limiting the amount of required memory.
         Only when the encoder and decoder are from the same architecture, can the flag 'tie_encoder_decoder' works.
         For more information, please refer to the TSDAE paper.
     """
+
     def __init__(
-        self, 
-        model: SentenceTransformer, 
-        decoder_name_or_path='bert-base-uncased', 
-        tie_encoder_decoder=True
+            self,
+            model: SentenceTransformer,
+            decoder_name_or_path: str = None,
+            tie_encoder_decoder: bool = True
     ):
         """
         :param model: SentenceTransformer model
@@ -33,25 +34,46 @@ class DenoisingAutoEncoderLoss(nn.Module):
         super(DenoisingAutoEncoderLoss, self).__init__()
         self.encoder = model  # This will be the final model used during the inference time.
         self.tokenizer_encoder = model.tokenizer
+
+        encoder_name_or_path = model[0].auto_model.config._name_or_path
+        if decoder_name_or_path is None:
+            assert tie_encoder_decoder, "Must indicate the decoder_name_or_path argument when tie_encoder_decoder=False!"
+        if tie_encoder_decoder:
+            if decoder_name_or_path:
+                logger.warning('When tie_encoder_decoder=True, the decoder_name_or_path will be invalid.')
+            decoder_name_or_path = encoder_name_or_path
+
         self.tokenizer_decoder = AutoTokenizer.from_pretrained(decoder_name_or_path)
-        self.need_retokenization = not(type(self.tokenizer_encoder) == type(self.tokenizer_decoder))
+        self.need_retokenization = not (type(self.tokenizer_encoder) == type(self.tokenizer_decoder))
 
         decoder_config = AutoConfig.from_pretrained(decoder_name_or_path)
         decoder_config.is_decoder = True
         decoder_config.add_cross_attention = True
         kwargs_decoder = {'config': decoder_config}
-        self.decoder = AutoModelForCausalLM.from_pretrained(decoder_name_or_path, **kwargs_decoder)
+        try:
+            self.decoder = AutoModelForCausalLM.from_pretrained(decoder_name_or_path, **kwargs_decoder)
+        except ValueError as e:
+            logger.error(f'Model name or path "{decoder_name_or_path}" does not support being as a decoder. Please make sure the decoder model has an "XXXLMHead" class.')
+            raise e
         assert model[0].auto_model.config.hidden_size == decoder_config.hidden_size, 'Hidden sizes do not match!'
         if self.tokenizer_decoder.pad_token is None:
             # Needed by GPT-2, etc.
             self.tokenizer_decoder.pad_token = self.tokenizer_decoder.eos_token
             self.decoder.config.pad_token_id = self.decoder.config.eos_token_id
 
-        if tie_encoder_decoder and not self.need_retokenization:
+        if len(AutoTokenizer.from_pretrained(encoder_name_or_path)) != len(self.tokenizer_encoder):
+            logger.warning('WARNING: The vocabulary of the encoder has been changed. One might need to change the decoder vocabulary, too.')
+
+        if tie_encoder_decoder:
+            assert not self.need_retokenization, "The tokenizers should be the same when tie_encoder_decoder=True."
+            if len(self.tokenizer_encoder) != len(self.tokenizer_decoder):  # The vocabulary has been changed.
+                self.tokenizer_decoder = self.tokenizer_encoder
+                self.decoder.resize_token_embeddings(len(self.tokenizer_decoder))
+                logger.warning('Since the encoder vocabulary has been changed and --tie_encoder_decoder=True, now the new vocabulary has also been used for the decoder.')
             decoder_base_model_prefix = self.decoder.base_model_prefix
             PreTrainedModel._tie_encoder_decoder_weights(
-                model[0].auto_model, 
-                self.decoder._modules[decoder_base_model_prefix], 
+                model[0].auto_model,
+                self.decoder._modules[decoder_base_model_prefix],
                 self.decoder.base_model_prefix
             )
 
@@ -59,15 +81,15 @@ class DenoisingAutoEncoderLoss(nn.Module):
         input_ids = sentence_features['input_ids']
         device = input_ids.device
         sentences_decoded = self.tokenizer_decoder.batch_decode(
-            input_ids, 
-            skip_special_tokens=True, 
+            input_ids,
+            skip_special_tokens=True,
             clean_up_tokenization_spaces=True
         )
         retokenized = self.tokenizer_decoder(
-            sentences_decoded, 
-            padding=True, 
-            truncation='longest_first', 
-            return_tensors="pt", 
+            sentences_decoded,
+            padding=True,
+            truncation='longest_first',
+            return_tensors="pt",
             max_length=None
         ).to(device)
         return retokenized
@@ -79,10 +101,10 @@ class DenoisingAutoEncoderLoss(nn.Module):
             # retokenization by the decoder's one is needed if different tokenizers used
             target_features = self.retokenize(target_features)
         reps = self.encoder(source_features)['sentence_embedding']  # (bsz, hdim)
-        
+
         # Prepare input and output
         target_length = target_features['input_ids'].shape[1]
-        decoder_input_ids = target_features['input_ids'].clone()[:, :target_length-1]
+        decoder_input_ids = target_features['input_ids'].clone()[:, :target_length - 1]
         label_ids = target_features['input_ids'][:, 1:]
 
         # Decode
@@ -91,7 +113,7 @@ class DenoisingAutoEncoderLoss(nn.Module):
             inputs_embeds=None,
             attention_mask=None,
             encoder_hidden_states=reps[:, None],  # (bsz, hdim) -> (bsz, 1, hdim)
-            encoder_attention_mask=source_features['attention_mask'][:, 0:1], 
+            encoder_attention_mask=source_features['attention_mask'][:, 0:1],
             labels=None,
             return_dict=None,
             use_cache=False
