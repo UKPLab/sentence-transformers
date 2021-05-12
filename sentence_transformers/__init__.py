a208d64806d99fb5aea0963ee5d3bd424b44b1cb
Nils Reimers <rnils@web.de>
2021-05-12 15:11:27 +0200

    bump version to 1.2.0

--- sentence_transformers/__init__.py
+++ sentence_transformers/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 __DOWNLOAD_SERVER__ = 'http://sbert.net/models/'
 from .datasets import SentencesDataset, ParallelSentencesDataset
 from .LoggingHandler import LoggingHandler
