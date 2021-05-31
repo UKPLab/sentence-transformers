12b06e75314166968b65644098c3e939ccc23856
Nils Reimers <rnils@web.de>
2021-05-31 21:22:34 +0200

    extract the compute metrics method

--- sentence_transformers/evaluation/InformationRetrievalEvaluator.py
+++ sentence_transformers/evaluation/InformationRetrievalEvaluator.py
@@ -87,16 +87,57 @@ class InformationRetrievalEvaluator(SentenceEvaluator):
             for k in map_at_k:
                 self.csv_headers.append("{}-MAP@{}".format(score_name, k))
 
-    def __call__(self, model, output_path: str = None, epoch: int = -1, steps: int = -1, corpus_model = None, corpus_embeddings: Tensor = None) -> float:
+    def __call__(self, model, output_path: str = None, epoch: int = -1, steps: int = -1, *args, **kwargs) -> float:
         if epoch != -1:
             out_txt = " after epoch {}:".format(epoch) if steps == -1 else " in epoch {} after {} steps:".format(epoch, steps)
         else:
             out_txt = ":"
 
+        logger.info("Information Retrieval Evaluation on " + self.name + " dataset" + out_txt)
+
+        scores = self.compute_metrices(model, *args, **kwargs)
+
+        # Write results to disc
+        if output_path is not None and self.write_csv:
+            csv_path = os.path.join(output_path, self.csv_file)
+            if not os.path.isfile(csv_path):
+                fOut = open(csv_path, mode="w", encoding="utf-8")
+                fOut.write(",".join(self.csv_headers))
+                fOut.write("\n")
+
+            else:
+                fOut = open(csv_path, mode="a", encoding="utf-8")
+
+            output_data = [epoch, steps]
+            for name in self.score_function_names:
+                for k in self.accuracy_at_k:
+                    output_data.append(scores[name]['accuracy@k'][k])
+
+                for k in self.precision_recall_at_k:
+                    output_data.append(scores[name]['precision@k'][k])
+                    output_data.append(scores[name]['recall@k'][k])
+
+                for k in self.mrr_at_k:
+                    output_data.append(scores[name]['mrr@k'][k])
+
+                for k in self.ndcg_at_k:
+                    output_data.append(scores[name]['ndcg@k'][k])
+
+                for k in self.map_at_k:
+                    output_data.append(scores[name]['map@k'][k])
+
+            fOut.write(",".join(map(str, output_data)))
+            fOut.write("\n")
+            fOut.close()
+
+        if self.main_score_function is None:
+            return max([scores[name]['map@k'][max(self.map_at_k)] for name in self.score_function_names])
+        else:
+            return scores[self.main_score_function]['map@k'][max(self.map_at_k)]
+
+    def compute_metrices(self, model, corpus_model = None, corpus_embeddings: Tensor = None) -> Dict[str, float]:
         if corpus_model is None:
             corpus_model = model
-
-        logger.info("Information Retrieval Evaluation on " + self.name + " dataset" + out_txt)
 
         max_k = max(max(self.mrr_at_k), max(self.ndcg_at_k), max(self.accuracy_at_k), max(self.precision_recall_at_k), max(self.map_at_k))
 
@@ -106,7 +147,6 @@ class InformationRetrievalEvaluator(SentenceEvaluator):
         queries_result_list = {}
         for name in self.score_functions:
             queries_result_list[name] = [[] for _ in range(len(query_embeddings))]
-
 
         #Iterate over chunks of the corpus
         for corpus_start_idx in trange(0, len(self.corpus), self.corpus_chunk_size, desc='Corpus Chunks', disable=not self.show_progress_bar):
@@ -143,45 +183,7 @@ class InformationRetrievalEvaluator(SentenceEvaluator):
             logging.info("Score-Function: {}".format(name))
             self.output_scores(scores[name])
 
-
-        #Write results to disc
-        if output_path is not None and self.write_csv:
-            csv_path = os.path.join(output_path, self.csv_file)
-            if not os.path.isfile(csv_path):
-                fOut = open(csv_path, mode="w", encoding="utf-8")
-                fOut.write(",".join(self.csv_headers))
-                fOut.write("\n")
-
-            else:
-                fOut = open(csv_path, mode="a", encoding="utf-8")
-
-            output_data = [epoch, steps]
-            for name in self.score_function_names:
-                for k in self.accuracy_at_k:
-                    output_data.append(scores[name]['accuracy@k'][k])
-
-                for k in self.precision_recall_at_k:
-                    output_data.append(scores[name]['precision@k'][k])
-                    output_data.append(scores[name]['recall@k'][k])
-
-                for k in self.mrr_at_k:
-                    output_data.append(scores[name]['mrr@k'][k])
-
-                for k in self.ndcg_at_k:
-                    output_data.append(scores[name]['ndcg@k'][k])
-
-                for k in self.map_at_k:
-                    output_data.append(scores[name]['map@k'][k])
-
-            fOut.write(",".join(map(str,output_data)))
-            fOut.write("\n")
-            fOut.close()
-
-        if self.main_score_function is None:
-            return max([scores[name]['map@k'][max(self.map_at_k)] for name in self.score_function_names])
-        else:
-            return scores[self.main_score_function]['map@k'][max(self.map_at_k)]
-
+        return scores
 
 
     def compute_metrics(self, queries_result_list: List[object]):
