4b9067e852d4522d0658b6ea495dcd991f8db70c
Nils Reimers <rnils@web.de>
2021-06-09 16:08:00 +0200

    update version numb

--- sentence_transformers/__init__.py
+++ sentence_transformers/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.0"
+__version__ = "2.0.0"
 __MODEL_HUB_ORGANIZATION__ = 'sentence-transformers'
 from .datasets import SentencesDataset, ParallelSentencesDataset
 from .LoggingHandler import LoggingHandler
