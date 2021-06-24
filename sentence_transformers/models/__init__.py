9ed90b636719c2812278b106ed9157727af41642
Nils Reimers <rnils@web.de>
2021-06-24 14:26:05 +0200

    remove WK pooling

--- sentence_transformers/models/__init__.py
+++ sentence_transformers/models/__init__.py
@@ -8,7 +8,6 @@ from .LayerNorm import LayerNorm
 from .LSTM import LSTM
 from .Normalize import Normalize
 from .Pooling import Pooling
-from .WKPooling import WKPooling
 from .WeightedLayerPooling import WeightedLayerPooling
 from .WordEmbeddings import WordEmbeddings
 from .WordWeights import WordWeights
