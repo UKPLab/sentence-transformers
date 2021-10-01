afee883a17ab039120783fd0cffe09ea979233cf
Nils Reimers <rnils@web.de>
2021-10-01 10:33:41 +0200

    update version to 2.1.0

--- examples/applications/image-search/example.py
+++ examples/applications/image-search/example.py
@@ -1,8 +1,39 @@
-from sentence_transformers import SentenceTransformer, util
-from PIL import Image
+from sentence_transformers import SentenceTransformer, util, models
+from PIL import ImageFile, Image
+import numpy as np
+import requests
+
+
+
+
+###########
+
+image = Image.open('two_dogs_in_snow.jpg')
+
+from transformers import CLIPProcessor, CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+
+
+inputs = processor(texts=["a cat", "a dog"], images=[image], return_tensors="pt", padding=True)
+output = model(**inputs)
+#vision_outputs = model.vision_model(pixel_values=inputs['pixel_values'])
+#image_embeds = model.visual_projection(vision_outputs[1])
+
+#print(image_embeds.shape)
+#exit()
+
+
 
 #Load CLIP model
-model = SentenceTransformer('clip-ViT-B-32')
+clip = models.CLIPModel()
+model = SentenceTransformer(modules=[clip])
+
+model.save('tmp-clip-model')
+
+model = SentenceTransformer('tmp-clip-model')
 
 #Encode an image:
 img_emb = model.encode(Image.open('two_dogs_in_snow.jpg'))
