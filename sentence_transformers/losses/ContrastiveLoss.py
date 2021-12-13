d2f0293987c11594972ab0ea3b71ed77d470698c
Nils Reimers <rnils@web.de>
2021-12-13 14:09:21 +0100

    Update ConstrativeLoss examples

--- sentence_transformers/losses/ContrastiveLoss.py
+++ sentence_transformers/losses/ContrastiveLoss.py
@@ -28,15 +28,18 @@ class ContrastiveLoss(nn.Module):
 
     Example::
 
-        from sentence_transformers import SentenceTransformer,  SentencesDataset, LoggingHandler, losses
-        from sentence_transformers.readers import InputExample
+        from sentence_transformers import SentenceTransformer, LoggingHandler, losses, InputExample
+        from torch.utils.data import DataLoader
 
-        model = SentenceTransformer('distilbert-base-nli-mean-tokens')
-        train_examples = [InputExample(texts=['This is a positive pair', 'Where the distance will be minimized'], label=1),
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+        train_examples = [
+            InputExample(texts=['This is a positive pair', 'Where the distance will be minimized'], label=1),
             InputExample(texts=['This is a negative pair', 'Their distance will be increased'], label=0)]
-        train_dataset = SentencesDataset(train_examples, model)
-        train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=train_batch_size)
+
+        train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=2)
         train_loss = losses.ContrastiveLoss(model=model)
+
+        model.fit([(train_dataloader, train_loss)], show_progress_bar=True)
 
     """
 
