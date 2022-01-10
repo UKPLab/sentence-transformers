147a870d93d897892045f86bf9fb101eac61b51b
Will Frey <jfrey89@gmail.com>
2022-01-10 15:06:23 -0500

    Update __init__.py
    Explicitly reexport the imported symbols in the top-level `__init__.py` file.

--- sentence_transformers/__init__.py
+++ sentence_transformers/__init__.py
@@ -1,8 +1,11 @@
 __version__ = "2.1.1"
 __MODEL_HUB_ORGANIZATION__ = 'sentence-transformers'
-from .datasets import SentencesDataset, ParallelSentencesDataset
-from .LoggingHandler import LoggingHandler
-from .SentenceTransformer import SentenceTransformer
-from .readers import InputExample
-from .cross_encoder.CrossEncoder import CrossEncoder
+from .datasets import (
+  SentencesDataset as SentencesDataset,
+  ParallelSentencesDataset as ParallelSentencesDataset,
+)
+from .LoggingHandler import LoggingHandler as LoggingHandler
+from .SentenceTransformer import SentenceTransformer as SentenceTransformer
+from .readers import InputExample as InputExample
+from .cross_encoder.CrossEncoder import CrossEncoder as CrossEncoder
 
