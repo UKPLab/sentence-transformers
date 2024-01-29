28be0cf151c466b89d1a1a5cf3e598e3932be6e1
Tom Aarsen <Cubiegamedev@gmail.com>
2024-01-29 13:17:44 +0100

    Run 'make style'

--- sentence_transformers/losses/CoSENTLoss.py
+++ sentence_transformers/losses/CoSENTLoss.py
@@ -4,16 +4,17 @@ from typing import Iterable, Dict
 from ..SentenceTransformer import SentenceTransformer
 from .. import util
 
+
 class CoSENTLoss(nn.Module):
     """
     This class implements CoSENT (Consistent Sentence Embedding via Similarity Ranking) loss.
     It expects that each of the InputExamples consists of a pair of texts and a float valued label, representing
     the expected similarity score between the pair.
-    
-    
+
+
     It computes the following loss function:
 
-    loss = logsum(1+exp(s(k,l)-s(i,j))+exp...), where (i,j) and (k,l) are any of the input pairs in the batch such that the expected 
+    loss = logsum(1+exp(s(k,l)-s(i,j))+exp...), where (i,j) and (k,l) are any of the input pairs in the batch such that the expected
     similarity of (i,j) is greater than (k,l). The summation is over all possible pairs of input pairs in the batch that match this condition.
 
     For further details, see: https://kexue.fm/archives/8847
@@ -34,8 +35,8 @@ class CoSENTLoss(nn.Module):
     +================================+========================+
     | (sentence_A, sentence_B) pairs | float similarity score |
     +--------------------------------+------------------------+
-    
-    
+
+
     Example::
 
         from sentence_transformers import SentenceTransformer, losses
@@ -44,23 +45,22 @@ class CoSENTLoss(nn.Module):
         model = SentenceTransformer('bert-base-uncased')
         train_examples = [InputExample(texts=['My first sentence', 'My second sentence'], label=1.0),
                 InputExample(texts=['My third sentence', 'Unrelated sentence'], label=0.3)]
-                
+
         train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=train_batch_size)
         train_loss = losses.CoSENTLoss(model=model)
     """
 
-    def __init__(self, model: SentenceTransformer, scale: float = 20.0, similarity_fct = util.pairwise_cos_sim):        
+    def __init__(self, model: SentenceTransformer, scale: float = 20.0, similarity_fct=util.pairwise_cos_sim):
         super(CoSENTLoss, self).__init__()
         self.model = model
         self.similarity_fct = similarity_fct
         self.scale = scale
 
-
     def forward(self, sentence_features: Iterable[Dict[str, Tensor]], labels: Tensor):
-        embeddings = [self.model(sentence_feature)['sentence_embedding'] for sentence_feature in sentence_features]
+        embeddings = [self.model(sentence_feature)["sentence_embedding"] for sentence_feature in sentence_features]
 
         scores = self.similarity_fct(embeddings[0], embeddings[1])
-        scores = scores*self.scale
+        scores = scores * self.scale
         scores = scores[:, None] - scores[None, :]
 
         # label matrix indicating which pairs are relevant
@@ -76,6 +76,5 @@ class CoSENTLoss(nn.Module):
 
         return loss
 
-    
     def get_config_dict(self):
         return {"scale": self.scale, "similarity_fct": self.similarity_fct.__name__}
