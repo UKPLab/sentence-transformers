235e7b7eea94f4f875f383edc0924a7ab1f0360b
Tom Aarsen <Cubiegamedev@gmail.com>
2024-01-31 14:15:33 +0100

    Slightly reformat the TripletLoss

--- sentence_transformers/losses/TripletLoss.py
+++ sentence_transformers/losses/TripletLoss.py
@@ -21,46 +21,48 @@ class TripletLoss(nn.Module):
     the loss minimizes the distance between anchor and positive while it maximizes the distance
     between anchor and negative. It compute the following loss function:
 
-    loss = max(||anchor - positive|| - ||anchor - negative|| + margin, 0).
+    ``loss = max(||anchor - positive|| - ||anchor - negative|| + margin, 0)``.
 
     Margin is an important hyperparameter and needs to be tuned respectively.
 
-    For further details, see: https://en.wikipedia.org/wiki/Triplet_loss
-
     :param model: SentenceTransformerModel
-    :param distance_metric: Function to compute distance between two embeddings. The class TripletDistanceMetric contains common distance metrices that can be used.
+    :param distance_metric: Function to compute distance between two embeddings. The class TripletDistanceMetric
+        contains common distance metrices that can be used.
     :param triplet_margin: The negative should be at least this much further away from the anchor than the positive.
+
+    References:
+        - For further details, see: https://en.wikipedia.org/wiki/Triplet_loss
 
     Requirements:
         1. (anchor, positive, negative) triplets
 
     Inputs:
+        +---------------------------------------+--------+
+        | Texts                                 | Labels |
+        +=======================================+========+
+        | (anchor, positive, negative) triplets | none   |
+        +---------------------------------------+--------+
 
-    +---------------------------------------+--------+
-    | Texts                                 | Labels |
-    +=======================================+========+
-    | (anchor, positive, negative) triplets |        |
-    +---------------------------------------+--------+
+    Example:
+        ::
 
-    Example::
+            from sentence_transformers import SentenceTransformer,  SentencesDataset, losses
+            from sentence_transformers.readers import InputExample
+            from torch.utils.data import DataLoader
 
-        from sentence_transformers import SentenceTransformer,  SentencesDataset, losses
-        from sentence_transformers.readers import InputExample
-        from torch.utils.data import DataLoader
-
-        model = SentenceTransformer('distilbert-base-nli-mean-tokens')
-        train_examples = [
-            InputExample(texts=['Anchor 1', 'Positive 1', 'Negative 1']),
-            InputExample(texts=['Anchor 2', 'Positive 2', 'Negative 2']),
-        ]
-        train_batch_size = 1
-        train_dataset = SentencesDataset(train_examples, model)
-        train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=train_batch_size)
-        train_loss = losses.TripletLoss(model=model)
-        model.fit(
-            [(train_dataloader, train_loss)],
-            epochs=10,
-        )
+            model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+            train_examples = [
+                InputExample(texts=['Anchor 1', 'Positive 1', 'Negative 1']),
+                InputExample(texts=['Anchor 2', 'Positive 2', 'Negative 2']),
+            ]
+            train_batch_size = 1
+            train_dataset = SentencesDataset(train_examples, model)
+            train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=train_batch_size)
+            train_loss = losses.TripletLoss(model=model)
+            model.fit(
+                [(train_dataloader, train_loss)],
+                epochs=10,
+            )
     """
 
     def __init__(
