1eec036cdcce649502de6152cb8a3b1ea1bb9a8c
Tom Aarsen <37621491+tomaarsen@users.noreply.github.com>
2024-02-15 11:11:57 +0100

    [`ci`] On Ubuntu CI runner, use temporary directories as cache folders for some models (#2481)
    * Use tempdir as cache_dir for Ubuntu CI tests
    
    * Skip slow training tests on CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import os
+import platform
+import tempfile
 import pytest
 
 from sentence_transformers import SentenceTransformer, CrossEncoder
@@ -30,3 +33,18 @@ def distilbert_base_uncased_model() -> SentenceTransformer:
     pooling_model = Pooling(word_embedding_model.get_word_embedding_dimension())
     model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
     return model
+
+
+@pytest.fixture()
+def cache_dir():
+    """
+    In the CI environment, we use a temporary directory as `cache_dir`
+    to avoid keeping the downloaded models on disk after the test.
+
+    This is only required for Ubuntu, as we otherwise have disk space issues there.
+    """
+    if os.environ.get("CI", None) and platform.system() == "Linux":
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            yield tmp_dir
+    else:
+        yield None
