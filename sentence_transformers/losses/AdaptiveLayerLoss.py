08826c30971dc49048406101cd53097113c5f52f
Tom Aarsen <Cubiegamedev@gmail.com>
2024-02-26 15:47:32 +0100

    Re-add KV-divergence, now on embeddings after pooling (etc.)

--- sentence_transformers/losses/AdaptiveLayerLoss.py
+++ sentence_transformers/losses/AdaptiveLayerLoss.py
@@ -1,5 +1,7 @@
+import random
 from typing import Dict, Iterable, List, Tuple
 from torch import Tensor, nn
+from torch.nn import functional as F
 import torch
 from sentence_transformers import SentenceTransformer
 from sentence_transformers.models import Transformer
@@ -63,46 +65,72 @@ class TransformerDecorator:
         return {**self.features[self.call_idx], "token_embeddings": self.embeddings[self.call_idx][self.layer_idx]}
 
 
+class ForwardDecorator:
+    def __init__(self, fn):
+        self.fn = fn
+        self.embeddings = []
+
+    def __call__(self, features):
+        output = self.fn(features)
+        self.embeddings.append(output["sentence_embedding"])
+        return output
+
+    def get_embeddings(self):
+        embeddings = torch.concat(self.embeddings, dim=0)
+        self.embeddings = []
+        return embeddings
+
+
 class AdaptiveLayerLoss(nn.Module):
     def __init__(
         self,
         model: SentenceTransformer,
         loss: nn.Module,
+        n_layers_per_step: int = -1,
     ):
         super().__init__()
         self.model = model
         self.loss = loss
+        self.n_layers_per_step = n_layers_per_step
         assert isinstance(self.model[0], Transformer)
 
     def forward(self, sentence_features: Iterable[Dict[str, Tensor]], labels: Tensor) -> Tensor:
-        original_forward = self.model[0].forward
-        decorated_forward = TransformerDecorator(self.model[0], original_forward)
-        self.model[0].forward = decorated_forward
+        # Decorate the forward function of the transformer to cache the embeddings of all layers
+        original_transformer_forward = self.model[0].forward
+        transformer_decorator = TransformerDecorator(self.model[0], original_transformer_forward)
+        self.model[0].forward = transformer_decorator
 
-        # Run the loss normally: i.e. the final layer
-        # but use the decorator to get the embeddings of all layers
+        # Decorate the forward function of the model to get the embeddings after all modules (e.g. pooling)
+        original_forward = self.model.forward
+        forward_decorator = ForwardDecorator(original_forward)
+        self.model.forward = forward_decorator
+
+        # Run the loss normally: i.e. the final layer, but 1) use the transformers decorator to cache
+        # the embeddings of all layers and 2) use the forward decorator to get the embeddings after all modules
+        # for the KL-divergence loss
         loss = self.loss(sentence_features, labels)
-        """
-        final_embeddings = torch.concat(decorated_forward.last_embeddings, dim=1)
-        """
+        final_embeddings = forward_decorator.get_embeddings()
+        final_embeddings = F.softmax(final_embeddings, dim=-1)
+
+        num_layers = transformer_decorator.num_layers
+        layer_indices = range(num_layers - 1)
+        if self.n_layers_per_step > 0 and self.n_layers_per_step < num_layers - 1:
+            layer_indices = random.sample(layer_indices, self.n_layers_per_step)
 
         # This loop is over `num_layer - 1` layers because we already computed the loss over the final layer
-        for layer_idx in range(decorated_forward.num_layers - 1):
+        for layer_idx in layer_indices:
             # Add regular loss for each layer by using the cached embeddings of that layer
-            decorated_forward.set_layer_idx(layer_idx)
-            layer_loss = self.loss(sentence_features, labels) / (decorated_forward.num_layers - 1)
-            loss = loss + layer_loss
+            transformer_decorator.set_layer_idx(layer_idx)
+            layer_loss = self.loss(sentence_features, labels)
+            loss = loss + layer_loss / len(layer_indices)
 
-            """
             # and KL-divergence loss between the current layer and the final layer
             # Note: we use "batchmean" reduction as that aligns with the mathematical definition
-            embeddings = decorated_forward.get_layer_embeddings()
-            kl_div_loss = F.kl_div(
-                F.log_softmax(embeddings, dim=-1), F.softmax(final_embeddings, dim=-1), reduction="batchmean"
-            ) / (decorated_forward.num_layers - 1)
-            loss = loss + kl_div_loss
-            """
+            embeddings = forward_decorator.get_embeddings()
+            kl_div_loss = F.kl_div(F.log_softmax(embeddings, dim=-1), final_embeddings, reduction="batchmean")
+            loss = loss + kl_div_loss / len(layer_indices)
 
-        self.model[0].forward = original_forward
+        self.model[0].forward = original_transformer_forward
+        self.model.forward = original_forward
 
         return loss
