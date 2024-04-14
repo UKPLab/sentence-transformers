c20de54beac66c2df9dd105e00b571c66d160601
James (Babel) <jamesjzhao01@gmail.com>
2024-04-13 23:22:59 -0400

    james script

---
+++ scripts/pull.sh
@@ -0,0 +1,32 @@
+mkdir -p data
+cd data
+if [ ! \( -d queries \) ]; then
+  echo "Pulling Queries"
+  mkdir -p queries
+  if [ ! \( -f queries.tar.gz \) ]; then
+    wget https://msmarco.z22.web.core.windows.net/msmarcoranking/queries.tar.gz
+  fi
+  tar -xvzf queries.tar.gz -C queries &&
+  rm queries.tar.gz
+fi
+if [ ! \( -d collection \) ]; then
+  echo "Pulling Collection"
+  mkdir -p collection
+  if [ ! \( -f collection.tar.gz \) ]; then
+    wget https://msmarco.z22.web.core.windows.net/msmarcoranking/collection.tar.gz
+  fi
+  tar -xvzf collection.tar.gz -C collection &&
+  rm collection.tar.gz
+fi
+if [ ! \( -d top1000 \) ]; then
+  echo "Pulling Collection"
+  mkdir -p top1000
+  if [ ! \( -f top1000.dev.tar.gz \) ]; then
+    wget https://msmarco.z22.web.core.windows.net/msmarcoranking/top1000.dev.tar.gz
+  fi
+  tar -xvzf top1000.tar.gz -C top1000 &&
+  rm top1000.dev.tar.gz
+fi
+# wget https://msmarco.z22.web.core.windows.net/msmarcoranking/queries.tar.gz
+# wget https://msmarco.z22.web.core.windows.net/msmarcoranking/collection.tar.gz
+# wget https://msmarco.z22.web.core.windows.net/msmarcoranking/top1000.dev.tar.gz
