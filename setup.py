57ea98cee1822ca7284905dc952c168b7f930f15
Max Friedrich <hallo@maxfriedrich.de>
2024-04-16 10:58:03 +0200

    Update transformers requirement in setup.py to match requirements.txt (#2589)
    see #2574

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(),
     python_requires=">=3.8.0",
     install_requires=[
-        "transformers>=4.32.0,<5.0.0",
+        "transformers>=4.34.0,<5.0.0",
         "tqdm",
         "torch>=1.11.0",
         "numpy",
