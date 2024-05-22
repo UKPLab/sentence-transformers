5f75ce5dcb39c8e74bcf629665bad9825f7d0452
satyamk7054 <43010011+satyamk7054@users.noreply.github.com>
2024-05-22 05:27:26 -0700

    Allow passing model_args to ST (#2578)
    * Use torch_dtype='auto' when loading auto-class
    
    * Use torch_dtype='auto' as default if model_args doesn't have it
    
    * Allow passing model_args for ST
    
    * Make same change for T5 and MT5
    
    * Update method documentation
    
    * Disable test if CUDA is not available
    
    * Add explicit dtype param to signature
    
    * Add model kwargs, update documentation, add more tests
    
    * Update based on suggestions
    
    * Format fixes
    
    * Update docstrings slightly
    
    * Reintroduce config kwargs; needed for token, trust_remote_code, revision, etc.
    
    * Propose dict kwargs for model, tokenizer & config
    
    * Remove dict defaults
    
    * Only cast to float() if necessary; adopted from v3.0-pre-release
    
    ---------
    
    Co-authored-by: Tom Aarsen <Cubiegamedev@gmail.com>

--- sentence_transformers/SentenceTransformer.py
+++ sentence_transformers/SentenceTransformer.py
@@ -5,7 +5,7 @@ import os
 import shutil
 from collections import OrderedDict
 import warnings
-from typing import List, Dict, Literal, Tuple, Iterable, Type, Union, Callable, Optional, TYPE_CHECKING
+from typing import List, Dict, Literal, Tuple, Iterable, Type, Union, Callable, Optional, TYPE_CHECKING, Any
 import numpy as np
 from numpy import ndarray
 import transformers
@@ -74,7 +74,40 @@ class SentenceTransformer(nn.Sequential):
     :param local_files_only: If `True`, avoid downloading the model.
     :param token: Hugging Face authentication token to download private models.
     :param truncate_dim: The dimension to truncate sentence embeddings to. `None` does no truncation. Truncation is
-        only applicable during inference when `.encode` is called.
+        only applicable during inference when :meth:`SentenceTransformer.encode` is called.
+    :param model_kwargs: Additional model configuration parameters to be passed to the Huggingface Transformers model.
+        Particularly useful options are:
+
+        - ``torch_dtype``: Override the default `torch.dtype` and load the model under a specific `dtype`.
+          The different options are:
+
+                1. ``torch.float16``, ``torch.bfloat16`` or ``torch.float``: load in a specified
+                ``dtype``, ignoring the model's ``config.torch_dtype`` if one exists. If not specified - the model will
+                get loaded in ``torch.float`` (fp32).
+
+                2. ``"auto"`` - A ``torch_dtype`` entry in the ``config.json`` file of the model will be
+                attempted to be used. If this entry isn't found then next check the ``dtype`` of the first weight in
+                the checkpoint that's of a floating point type and use that as ``dtype``. This will load the model
+                using the ``dtype`` it was saved in at the end of the training. It can't be used as an indicator of how
+                the model was trained. Since it could be trained in one of half precision dtypes, but saved in fp32.
+        - ``attn_implementation``: The attention implementation to use in the model (if relevant). Can be any of
+          `"eager"` (manual implementation of the attention), `"sdpa"` (using `F.scaled_dot_product_attention
+          <https://pytorch.org/docs/master/generated/torch.nn.functional.scaled_dot_product_attention.html>`_),
+          or `"flash_attention_2"` (using `Dao-AILab/flash-attention <https://github.com/Dao-AILab/flash-attention>`_).
+          By default, if available, SDPA will be used for torch>=2.1.1. The default is otherwise the manual `"eager"`
+          implementation.
+
+        See the `PreTrainedModel.from_pretrained
+        <https://huggingface.co/docs/transformers/en/main_classes/model#transformers.PreTrainedModel.from_pretrained>`_
+        documentation for more details.
+    :param tokenizer_kwargs: Additional tokenizer configuration parameters to be passed to the Huggingface Transformers tokenizer.
+        See the `AutoTokenizer.from_pretrained
+        <https://huggingface.co/docs/transformers/en/model_doc/auto#transformers.AutoTokenizer.from_pretrained>`_
+        documentation for more details.
+    :param config_kwargs: Additional model configuration parameters to be passed to the Huggingface Transformers config.
+        See the `AutoConfig.from_pretrained
+        <https://huggingface.co/docs/transformers/en/model_doc/auto#transformers.AutoConfig.from_pretrained>`_
+        documentation for more details.
     """
 
     def __init__(
@@ -91,6 +124,9 @@ class SentenceTransformer(nn.Sequential):
         token: Optional[Union[bool, str]] = None,
         use_auth_token: Optional[Union[bool, str]] = None,
         truncate_dim: Optional[int] = None,
+        model_kwargs: Optional[Dict[str, Any]] = None,
+        tokenizer_kwargs: Optional[Dict[str, Any]] = None,
+        config_kwargs: Optional[Dict[str, Any]] = None,
     ):
         # Note: self._load_sbert_model can also update `self.prompts` and `self.default_prompt_name`
         self.prompts = prompts or {}
@@ -220,6 +256,9 @@ class SentenceTransformer(nn.Sequential):
                     revision=revision,
                     trust_remote_code=trust_remote_code,
                     local_files_only=local_files_only,
+                    model_kwargs=model_kwargs,
+                    tokenizer_kwargs=tokenizer_kwargs,
+                    config_kwargs=config_kwargs,
                 )
             else:
                 modules = self._load_auto_model(
@@ -229,6 +268,9 @@ class SentenceTransformer(nn.Sequential):
                     revision=revision,
                     trust_remote_code=trust_remote_code,
                     local_files_only=local_files_only,
+                    model_kwargs=model_kwargs,
+                    tokenizer_kwargs=tokenizer_kwargs,
+                    config_kwargs=config_kwargs,
                 )
 
         if modules is not None and not isinstance(modules, OrderedDict):
@@ -461,7 +503,10 @@ class SentenceTransformer(nn.Sequential):
                 all_embeddings = torch.Tensor()
         elif convert_to_numpy:
             if not isinstance(all_embeddings, np.ndarray):
-                all_embeddings = np.asarray([emb.numpy() for emb in all_embeddings])
+                if all_embeddings[0].dtype == torch.bfloat16:
+                    all_embeddings = np.asarray([emb.float().numpy() for emb in all_embeddings])
+                else:
+                    all_embeddings = np.asarray([emb.numpy() for emb in all_embeddings])
         elif isinstance(all_embeddings, np.ndarray):
             all_embeddings = [torch.from_numpy(embedding) for embedding in all_embeddings]
 
@@ -1235,30 +1280,35 @@ class SentenceTransformer(nn.Sequential):
         revision: Optional[str] = None,
         trust_remote_code: bool = False,
         local_files_only: bool = False,
+        model_kwargs: Optional[Dict[str, Any]] = None,
+        tokenizer_kwargs: Optional[Dict[str, Any]] = None,
+        config_kwargs: Optional[Dict[str, Any]] = None,
     ):
         """
         Creates a simple Transformer + Mean Pooling model and returns the modules
         """
         logger.warning(
-            "No sentence-transformers model found with name {}. Creating a new one with MEAN pooling.".format(
+            "No sentence-transformers model found with name {}. Creating a new one with mean pooling.".format(
                 model_name_or_path
             )
         )
+
+        shared_kwargs = {
+            "token": token,
+            "trust_remote_code": trust_remote_code,
+            "revision": revision,
+            "local_files_only": local_files_only,
+        }
+        model_kwargs = shared_kwargs if model_kwargs is None else {**shared_kwargs, **model_kwargs}
+        tokenizer_kwargs = shared_kwargs if tokenizer_kwargs is None else {**shared_kwargs, **tokenizer_kwargs}
+        config_kwargs = shared_kwargs if config_kwargs is None else {**shared_kwargs, **config_kwargs}
+
         transformer_model = Transformer(
             model_name_or_path,
             cache_dir=cache_folder,
-            model_args={
-                "token": token,
-                "trust_remote_code": trust_remote_code,
-                "revision": revision,
-                "local_files_only": local_files_only,
-            },
-            tokenizer_args={
-                "token": token,
-                "trust_remote_code": trust_remote_code,
-                "revision": revision,
-                "local_files_only": local_files_only,
-            },
+            model_args=model_kwargs,
+            tokenizer_args=tokenizer_kwargs,
+            config_args=config_kwargs,
         )
         pooling_model = Pooling(transformer_model.get_word_embedding_dimension(), "mean")
         return [transformer_model, pooling_model]
@@ -1271,6 +1321,9 @@ class SentenceTransformer(nn.Sequential):
         revision: Optional[str] = None,
         trust_remote_code: bool = False,
         local_files_only: bool = False,
+        model_kwargs: Optional[Dict[str, Any]] = None,
+        tokenizer_kwargs: Optional[Dict[str, Any]] = None,
+        config_kwargs: Optional[Dict[str, Any]] = None,
     ):
         """
         Loads a full sentence-transformers model
@@ -1360,21 +1413,42 @@ class SentenceTransformer(nn.Sequential):
                     if config_path is not None:
                         with open(config_path) as fIn:
                             kwargs = json.load(fIn)
+                            # Don't allow configs to set trust_remote_code
+                            if "model_args" in kwargs and "trust_remote_code" in kwargs["model_args"]:
+                                kwargs["model_args"].pop("trust_remote_code")
+                            if "tokenizer_args" in kwargs and "trust_remote_code" in kwargs["tokenizer_args"]:
+                                kwargs["tokenizer_args"].pop("trust_remote_code")
+                            if "config_args" in kwargs and "trust_remote_code" in kwargs["config_args"]:
+                                kwargs["config_args"].pop("trust_remote_code")
                         break
+
                 hub_kwargs = {
                     "token": token,
                     "trust_remote_code": trust_remote_code,
                     "revision": revision,
                     "local_files_only": local_files_only,
                 }
-                if "model_args" in kwargs:
-                    kwargs["model_args"].update(hub_kwargs)
-                else:
-                    kwargs["model_args"] = hub_kwargs
-                if "tokenizer_args" in kwargs:
-                    kwargs["tokenizer_args"].update(hub_kwargs)
-                else:
-                    kwargs["tokenizer_args"] = hub_kwargs
+                # 3rd priority: config file
+                if "model_args" not in kwargs:
+                    kwargs["model_args"] = {}
+                if "tokenizer_args" not in kwargs:
+                    kwargs["tokenizer_args"] = {}
+                if "config_args" not in kwargs:
+                    kwargs["config_args"] = {}
+
+                # 2nd priority: hub_kwargs
+                kwargs["model_args"].update(hub_kwargs)
+                kwargs["tokenizer_args"].update(hub_kwargs)
+                kwargs["config_args"].update(hub_kwargs)
+
+                # 1st priority: kwargs passed to SentenceTransformer
+                if model_kwargs:
+                    kwargs["model_args"].update(model_kwargs)
+                if tokenizer_kwargs:
+                    kwargs["tokenizer_args"].update(tokenizer_kwargs)
+                if config_kwargs:
+                    kwargs["config_args"].update(config_kwargs)
+
                 module = Transformer(model_name_or_path, cache_dir=cache_folder, **kwargs)
             else:
                 # Normalize does not require any files to be loaded
