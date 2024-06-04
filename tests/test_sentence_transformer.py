0f84a7bcc090214e361c21ddfb00387b151379ea
Tom Aarsen <Cubiegamedev@gmail.com>
2024-06-04 22:04:31 +0200

    Replace 'sentence_transformers_model_id' from reused model if possible

--- tests/test_sentence_transformer.py
+++ tests/test_sentence_transformer.py
@@ -564,3 +564,24 @@ def test_similarity_score_save(stsb_bert_tiny_model: SentenceTransformer) -> Non
     assert loaded_model.similarity_fn_name == "euclidean"
     dot_scores = model.similarity(embeddings, embeddings)
     assert np.not_equal(cosine_scores, dot_scores).all()
+
+def test_model_card_save_update_model_id(stsb_bert_tiny_model: SentenceTransformer) -> None:
+    model = stsb_bert_tiny_model
+    # Removing the saved model card will cause a fresh one to be generated when we save
+    model._model_card_text = ""
+    with tempfile.TemporaryDirectory() as tmp_folder:
+        model.save(tmp_folder)
+        with open(Path(tmp_folder) / "README.md", "r", encoding="utf8") as f:
+            model_card_text = f.read()
+            assert 'model = SentenceTransformer("sentence_transformers_model_id"' in model_card_text
+
+        # When we reload this saved model and then re-save it, we want to override the 'sentence_transformers_model_id'
+        # if we have it set
+        loaded_model = SentenceTransformer(tmp_folder)
+        
+    with tempfile.TemporaryDirectory() as tmp_folder:
+        loaded_model.save(tmp_folder, model_name="test_user/test_model")
+
+        with open(Path(tmp_folder) / "README.md", "r", encoding="utf8") as f:
+            model_card_text = f.read()
+            assert 'model = SentenceTransformer("test_user/test_model"' in model_card_text
