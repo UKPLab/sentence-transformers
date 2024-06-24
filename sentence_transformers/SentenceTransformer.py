7078441d96ad9213395ddefd348d038bf7d1ebd6
Tom Aarsen <Cubiegamedev@gmail.com>
2024-06-24 14:01:30 +0200

    Fall back to module.load if kwargs are not accepted

--- sentence_transformers/SentenceTransformer.py
+++ sentence_transformers/SentenceTransformer.py
@@ -1532,7 +1532,12 @@ class SentenceTransformer(nn.Sequential, FitMixin):
                 if config_kwargs:
                     kwargs["config_args"].update(config_kwargs)
 
-                module = module_class(model_name_or_path, cache_dir=cache_folder, **kwargs)
+                # Try to initialize the module with a lot of kwargs, but only if the module supports them
+                # Otherwise we fall back to the load method
+                try:
+                    module = module_class(model_name_or_path, cache_dir=cache_folder, **kwargs)
+                except TypeError:
+                    module = module_class.load(model_name_or_path)
             else:
                 # Normalize does not require any files to be loaded
                 if module_class == Normalize:
