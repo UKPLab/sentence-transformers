082f064045d850879150cfe908601cf943bcfcbb
Tom Aarsen <Cubiegamedev@gmail.com>
2024-06-25 09:35:38 +0200

    Fix retokenization on DDP/DP with GIST losses

--- sentence_transformers/losses/CachedGISTEmbedLoss.py
+++ sentence_transformers/losses/CachedGISTEmbedLoss.py
@@ -149,6 +149,8 @@ class CachedGISTEmbedLoss(nn.Module):
         self.must_retokenize = (
             model.tokenizer.vocab != guide.tokenizer.vocab or guide.max_seq_length < model.max_seq_length
         )
+        if self.must_retokenize:
+            self.guide_tokenize_fn = guide.tokenize
 
     def sim_matrix(self, embed1: Tensor, embed2: Tensor) -> Tensor:
         return self.similarity_fct(embed1.unsqueeze(1), embed2.unsqueeze(0))
@@ -175,7 +177,7 @@ class CachedGISTEmbedLoss(nn.Module):
                     decoded = self.model.tokenizer.batch_decode(
                         sentence_feature_minibatch["input_ids"], skip_special_tokens=True
                     )
-                    sentence_feature_minibatch = self.guide.tokenize(decoded)
+                    sentence_feature_minibatch = self.guide_tokenize_fn(decoded)
                     sentence_feature_minibatch = {
                         key: value.to(self.guide.device) for key, value in sentence_feature_minibatch.items()
                     }
