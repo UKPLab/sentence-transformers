921774ef96000ca5df22703a0c72579ba50ee877
Florian Maas <fpgmaas@gmail.com>
2024-06-30 17:48:04 +0200

    fix test

--- tests/samplers/test_round_robin_batch_sampler.py
+++ tests/samplers/test_round_robin_batch_sampler.py
@@ -4,6 +4,8 @@ from sentence_transformers.sampler import RoundRobinBatchSampler
 
 from torch.utils.data import BatchSampler, SequentialSampler, ConcatDataset
 
+DATASET_LENGTH = 25
+
 
 @pytest.fixture
 def dummy_dataset() -> ConcatDataset:
@@ -14,8 +16,7 @@ def dummy_dataset() -> ConcatDataset:
         "label": [0, 1, 0, 1, ..., 0, 1],
     }
     """
-
-    values_1 = list(range(25))
+    values_1 = list(range(DATASET_LENGTH))
     labels = [x % 2 for x in values_1]
     dataset_1 = Dataset.from_dict({"data": values_1, "label": labels})
 
@@ -26,12 +27,25 @@ def dummy_dataset() -> ConcatDataset:
 
 
 def test_round_robin_batch_sampler(dummy_dataset):
-    batch_sampler_1 = BatchSampler(SequentialSampler(range(len(dummy_dataset))), batch_size=4, drop_last=True)
-    batch_sampler_2 = BatchSampler(SequentialSampler(range(len(dummy_dataset))), batch_size=4, drop_last=True)
-    sampler = RoundRobinBatchSampler(dataset=dummy_dataset, batch_samplers=[batch_sampler_1, batch_sampler_2])
+    batch_size = 4
+    batch_sampler_1 = BatchSampler(SequentialSampler(range(DATASET_LENGTH)), batch_size=batch_size, drop_last=True)
+    batch_sampler_2 = BatchSampler(SequentialSampler(range(DATASET_LENGTH)), batch_size=batch_size, drop_last=True)
 
+    sampler = RoundRobinBatchSampler(dataset=dummy_dataset, batch_samplers=[batch_sampler_1, batch_sampler_2])
     batches = list(iter(sampler))
 
-    for batch in batches:
-        for idx in batch:
-            print(dummy_dataset[idx])
+    assert len(batches) == 2 * DATASET_LENGTH // batch_size
+
+    # Assert that batches are produced in a round-robin fashion
+    for i in range(0, len(batches), 2):
+        # Batch from the first part of the dataset
+        batch_1 = batches[i]
+        assert all(
+            dummy_dataset[idx]["data"] < 100 for idx in batch_1
+        ), f"Batch {i} contains data from the second part of the dataset: {[dummy_dataset[idx]['data'] for idx in batch_1]}"
+
+        # Batch from the second part of the dataset
+        batch_2 = batches[i + 1]
+        assert all(
+            dummy_dataset[idx]["data"] >= 100 for idx in batch_2
+        ), f"Batch {i+1} contains data from the first part of the dataset: {[dummy_dataset[idx]['data'] for idx in batch_2]}"
