39f1470c2451ca2c52f52d930d04074a57390d75
Tom Aarsen <Cubiegamedev@gmail.com>
2024-09-10 16:44:56 +0200

    Use clearer variable names

--- sentence_transformers/data_collator.py
+++ sentence_transformers/data_collator.py
@@ -18,25 +18,25 @@ class SentenceTransformerDataCollator:
     valid_label_columns: list[str] = field(default_factory=lambda: ["label", "score"])
 
     def __call__(self, features: list[dict[str, Any]]) -> dict[str, torch.Tensor]:
-        columns = list(features[0].keys())
+        column_names = list(features[0].keys())
 
         # We should always be able to return a loss, label or not:
         batch = {}
 
-        if "dataset_name" in columns:
-            columns.remove("dataset_name")
+        if "dataset_name" in column_names:
+            column_names.remove("dataset_name")
             batch["dataset_name"] = features[0]["dataset_name"]
 
         # Extract the label column if it exists
         for label_column in self.valid_label_columns:
-            if label_column in columns:
+            if label_column in column_names:
                 batch["label"] = torch.tensor([row[label_column] for row in features])
-                columns.remove(label_column)
+                column_names.remove(label_column)
                 break
 
         # Extract the feature columns
-        for column in columns:
-            tokenized = self.tokenize_fn([row[column] for row in features])
+        for column_name in column_names:
+            tokenized = self.tokenize_fn([row[column_name] for row in features])
             for key, value in tokenized.items():
-                batch[f"{column}_{key}"] = value
+                batch[f"{column_name}_{key}"] = value
         return batch
