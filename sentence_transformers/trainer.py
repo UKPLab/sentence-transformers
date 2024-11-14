5716f954552c58b67dff1a3d7d0eb9e37c2c2c2b
GTimothee <39728445+GTimothee@users.noreply.github.com>
2024-11-14 19:16:44 +0100

    Update trainer.py

--- sentence_transformers/trainer.py
+++ sentence_transformers/trainer.py
@@ -498,8 +498,8 @@ class SentenceTransformerTrainer(Trainer):
         if not isinstance(self.model[0], Transformer):
             logger.info("Could not load best model, as the model is not a `transformers`-based model.")
             return
-        elif isinstance(self.model[0].auto_model, PeftAdapterMixin):
-            logger.info("Could not load best model, as the model is a `PeftAdapterMixin`-based model. Please wait for an update of the transformers library to enable this feature.")
+        elif len(self.model[0].auto_model.active_adapters()):
+            logger.info("Could not load best model, as the model has at least one adapter set. Please wait for an update of the transformers library to enable this feature.")
             return
 
         try:
