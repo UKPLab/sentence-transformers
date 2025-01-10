c837a69f9a17679a0f4f19f343b7f3545229e389
Tom Aarsen <Cubiegamedev@gmail.com>
2025-01-10 13:35:26 +0100

    Reintroduce the nice "raises" tests

--- tests/evaluation/test_nanobeir_evaluator.py
+++ tests/evaluation/test_nanobeir_evaluator.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+import re
+
+import pytest
+
 from sentence_transformers import SentenceTransformer
 from sentence_transformers.evaluation import NanoBEIREvaluator
 
@@ -25,18 +29,21 @@ def test_nanobeir_evaluator():
     assert all(isinstance(results[metric], float) for metric in results)
 
 
-# def test_nanobeir_evaluator_with_invalid_dataset():
-#     """Test that NanoBEIREvaluator raises an error for invalid dataset names."""
-#     invalid_datasets = ["invalidDataset"]
+def test_nanobeir_evaluator_with_invalid_dataset():
+    """Test that NanoBEIREvaluator raises an error for invalid dataset names."""
+    invalid_datasets = ["invalidDataset"]
 
-#     with pytest.raises(ValueError, match=f"Dataset(s) {invalid_datasets} not found in the NanoBEIR collection.Valid dataset names are: ['climatefever', 'dbpedia', 'fever', 'fiqa2018', 'hotpotqa', 'msmarco', 'nfcorpus', 'nq', 'quoraretrieval', 'scidocs', 'arguana', 'scifact', 'touche2020']"):
-#         NanoBEIREvaluator(
-#             dataset_names=invalid_datasets,
-#         )
+    with pytest.raises(
+        ValueError,
+        match=re.escape(
+            r"Dataset(s) ['invalidDataset'] not found in the NanoBEIR collection."
+            r"Valid dataset names are: ['climatefever', 'dbpedia', 'fever', 'fiqa2018', 'hotpotqa', 'msmarco', 'nfcorpus', 'nq', 'quoraretrieval', 'scidocs', 'arguana', 'scifact', 'touche2020']"
+        ),
+    ):
+        NanoBEIREvaluator(dataset_names=invalid_datasets)
 
-# def test_nanobeir_evaluator_empty_inputs():
-#     """Test that NanoBEIREvaluator behaves correctly with empty datasets."""
-#     with pytest.raises(ValueError, match="dataset_names cannot be empty. Use None to evaluate on all datasets."):
-#         NanoBEIREvaluator(
-#             dataset_names=[],
-#         )
+
+def test_nanobeir_evaluator_empty_inputs():
+    """Test that NanoBEIREvaluator behaves correctly with empty datasets."""
+    with pytest.raises(ValueError, match="dataset_names cannot be empty. Use None to evaluate on all datasets."):
+        NanoBEIREvaluator(dataset_names=[])
