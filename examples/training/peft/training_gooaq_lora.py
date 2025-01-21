57395b4b944e71269dfb31a7f4e7c820cb91b24e
Tom Aarsen <37621491+tomaarsen@users.noreply.github.com>
2025-01-21 14:13:24 +0100

    [`docs`] Add PEFT documentation + training example (#3180)
    * Add PEFT documentation
    
    * Add details on model performances

---
+++ examples/training/peft/training_gooaq_lora.py
@@ -0,0 +1,111 @@
+import logging
+import sys
+import traceback
+
+from datasets import Dataset, load_dataset
+from peft import LoraConfig, TaskType
+
+from sentence_transformers import (
+    SentenceTransformer,
+    SentenceTransformerModelCardData,
+    SentenceTransformerTrainer,
+    SentenceTransformerTrainingArguments,
+)
+from sentence_transformers.evaluation import NanoBEIREvaluator
+from sentence_transformers.losses import MultipleNegativesRankingLoss
+from sentence_transformers.training_args import BatchSamplers
+
+# Set the log level to INFO to get more information
+logging.basicConfig(format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
+
+# You can specify any Hugging Face pre-trained model here, for example, bert-base-uncased, roberta-base, xlm-roberta-base
+model_name = sys.argv[1] if len(sys.argv) > 1 else "bert-base-uncased"
+model_name_only = model_name.split("/")[-1]
+
+# 1. Load a model to finetune with 2. (Optional) model card data
+model = SentenceTransformer(
+    model_name,
+    model_card_data=SentenceTransformerModelCardData(
+        language="en",
+        license="apache-2.0",
+        model_name=f"{model_name_only} adapter finetuned on GooAQ pairs",
+    ),
+)
+
+# Create a LoRA adapter for the model
+peft_config = LoraConfig(
+    task_type=TaskType.FEATURE_EXTRACTION,
+    inference_mode=False,
+    r=64,
+    lora_alpha=128,
+    lora_dropout=0.1,
+)
+model.add_adapter(peft_config)
+
+# 3. Load a dataset to finetune on
+dataset = load_dataset("sentence-transformers/gooaq", split="train")
+dataset_dict = dataset.train_test_split(test_size=10_000, seed=12)
+train_dataset: Dataset = dataset_dict["train"].select(range(1_000_000))
+eval_dataset: Dataset = dataset_dict["test"]
+
+# 4. Define a loss function
+loss = MultipleNegativesRankingLoss(model)
+
+# 5. (Optional) Specify training arguments
+run_name = f"{model_name_only}-gooaq-peft"
+args = SentenceTransformerTrainingArguments(
+    # Required parameter:
+    output_dir=f"models/{run_name}",
+    # Optional training parameters:
+    num_train_epochs=1,
+    per_device_train_batch_size=1024,
+    per_device_eval_batch_size=1024,
+    learning_rate=2e-5,
+    warmup_ratio=0.1,
+    fp16=False,  # Set to False if you get an error that your GPU can't run on FP16
+    bf16=True,  # Set to True if you have a GPU that supports BF16
+    batch_sampler=BatchSamplers.NO_DUPLICATES,  # MultipleNegativesRankingLoss benefits from no duplicate samples in a batch
+    # Optional tracking/debugging parameters:
+    eval_strategy="steps",
+    eval_steps=100,
+    save_strategy="steps",
+    save_steps=100,
+    save_total_limit=2,
+    logging_steps=25,
+    logging_first_step=True,
+    run_name=run_name,  # Will be used in W&B if `wandb` is installed
+)
+
+# 6. (Optional) Create an evaluator & evaluate the base model
+# The full corpus, but only the evaluation queries
+dev_evaluator = NanoBEIREvaluator(batch_size=1024)
+dev_evaluator(model)
+
+# 7. Create a trainer & train
+trainer = SentenceTransformerTrainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    loss=loss,
+    evaluator=dev_evaluator,
+)
+trainer.train()
+
+# (Optional) Evaluate the trained model on the evaluator after training
+dev_evaluator(model)
+
+# 8. Save the trained model
+final_output_dir = f"models/{run_name}/final"
+model.save_pretrained(final_output_dir)
+
+# 9. (Optional) save the model to the Hugging Face Hub!
+# It is recommended to run `huggingface-cli login` to log into your Hugging Face account first
+try:
+    model.push_to_hub(run_name)
+except Exception:
+    logging.error(
+        f"Error uploading model to the Hugging Face Hub:\n{traceback.format_exc()}To upload it manually, you can run "
+        f"`huggingface-cli login`, followed by loading the model using `model = SentenceTransformer({final_output_dir!r})` "
+        f"and saving it using `model.push_to_hub('{run_name}')`."
+    )
