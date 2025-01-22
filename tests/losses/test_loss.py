25b87976cb23278cfbe3342b1d9989c614978c77
JINO-ROHIT <find.jinorohit@gmail.com>
2025-01-22 18:37:38 +0530

    softmax loss

--- tests/losses/test_loss.py
+++ tests/losses/test_loss.py
@@ -11,6 +11,7 @@ from sentence_transformers.losses import (
     CachedMultipleNegativesRankingLoss,
     GISTEmbedLoss,
     MultipleNegativesRankingLoss,
+    SoftmaxLoss,
     TripletLoss,
 )
 from sentence_transformers.util import batch_to_device
@@ -29,6 +30,7 @@ def get_anchor_positive_negative_triplet():
             (TripletLoss, {}),
             (CachedGISTEmbedLoss, {"guide": "GUIDE_MODEL_PLACEHOLDER"}),
             (GISTEmbedLoss, {"guide": "GUIDE_MODEL_PLACEHOLDER"}),
+            (SoftmaxLoss, {"num_labels": 3}),
         ],
         "correct": Dataset.from_dict(
             {
@@ -44,34 +46,67 @@ def get_anchor_positive_negative_triplet():
                 "negative": ["The sun is out today", "I like playing soccer", "I am studying at university"],
             }
         ),
+        "softmax_correct": Dataset.from_dict(
+            {
+                "sentence1": ["It's very sunny outside", "I love playing soccer", "I am a student"],
+                "sentence2": ["The sun is out today", "I like playing soccer", "I am studying at university"],
+                "label": [1, 1, 0],
+            }
+        ),
+        "softmax_incorrect": Dataset.from_dict(
+            {
+                "sentence1": ["It's very sunny outside", "I love playing soccer", "I am a student"],
+                "sentence2": ["Data science is fun", "Cacti are beautiful", "Speakers are loud"],
+                "label": [2, 2, 1],
+            }
+        ),
     }
 
 
 def get_loss_test_cases():
     anchor_positive_negative_triplet = get_anchor_positive_negative_triplet()
-    return [
-        (
-            loss_class,
-            loss_args,
-            anchor_positive_negative_triplet["correct"],
-            anchor_positive_negative_triplet["incorrect"],
-        )
-        for loss_class, loss_args in anchor_positive_negative_triplet["losses"]
-    ]
+    test_cases = []
+    for loss_class, loss_args in anchor_positive_negative_triplet["losses"]:
+        if loss_class == SoftmaxLoss:
+            loss_args["sentence_embedding_dimension"] = 384
+            test_cases.append(
+                (
+                    loss_class,
+                    loss_args,
+                    anchor_positive_negative_triplet["softmax_correct"],
+                    anchor_positive_negative_triplet["softmax_incorrect"],
+                )
+            )
+        else:
+            test_cases.append(
+                (
+                    loss_class,
+                    loss_args,
+                    anchor_positive_negative_triplet["correct"],
+                    anchor_positive_negative_triplet["incorrect"],
+                )
+            )
+    return test_cases
 
 
 def prepare_features_labels_from_dataset(model: SentenceTransformer, dataset: Dataset):
     device = model.device
-    features = [
-        batch_to_device(model.tokenize(dataset[column]), device)
-        for column in dataset.column_names
-        if column not in ["label", "score"]
-    ]
-    labels = None
-    if "label" in dataset.column_names:
+    if "sentence1" in dataset.column_names and "sentence2" in dataset.column_names:
+        # Handle SoftmaxLoss case
+        features = [batch_to_device(model.tokenize(dataset[column]), device) for column in ["sentence1", "sentence2"]]
         labels = torch.tensor(dataset["label"]).to(device)
-    elif "score" in dataset.column_names:
-        labels = torch.tensor(dataset["score"]).to(device)
+    else:
+        # Handle other losses
+        features = [
+            batch_to_device(model.tokenize(dataset[column]), device)
+            for column in dataset.column_names
+            if column not in ["label", "score"]
+        ]
+        labels = None
+        if "label" in dataset.column_names:
+            labels = torch.tensor(dataset["label"]).to(device)
+        elif "score" in dataset.column_names:
+            labels = torch.tensor(dataset["score"]).to(device)
     return features, labels
 
 
@@ -93,6 +128,8 @@ def test_loss_function(
         loss_args["guide"] = guide_model
 
     model = stsb_bert_tiny_model_reused
+    if loss_class == SoftmaxLoss:
+        loss_args["sentence_embedding_dimension"] = model.get_sentence_embedding_dimension()
     loss_fn = loss_class(model, **loss_args)
     correct_loss = get_and_assert_loss_from_dataset(model, loss_fn, correct)
     incorrect_loss = get_and_assert_loss_from_dataset(model, loss_fn, incorrect)
