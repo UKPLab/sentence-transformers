be7f09695600c7dc51ad88fb97a187ac9e8ebab6
arthurbr11 <arthurb242@icloud.com>
2025-05-16 11:25:26 +0200

    Add training script for Quora Duplicate Questions and update model upload logging

--- examples/sparse_encoder/training/quora_duplicate_questions/training_splade_quora.py
+++ examples/sparse_encoder/training/quora_duplicate_questions/training_splade_quora.py
@@ -1,12 +1,14 @@
 """
-This example trains a SparseEncoder for the GooAQ dataset.
-The training script fine-tunes a SparseEncoder using the Splade loss function for retrieval.
-It loads a subset of the GooAQ dataset, splits it into training and evaluation subsets,
-and trains the model as a retriever. After training, the model is evaluated and saved locally,
-with an optional step to push the trained model to the Hugging Face Hub.
+This scripts demonstrates how to train a sentence embedding model for Information Retrieval.
 
-Usage:
-python train_splade_gooaq.py
+As dataset, we use Quora Duplicates Questions, where we have pairs of duplicate questions.
+
+As loss function, we use MultipleNegativesRankingLoss. Here, we only need positive pairs, i.e., pairs of sentences/texts that are considered to be relevant. Our dataset looks like this (a_1, b_1), (a_2, b_2), ... with a_i / b_i a text and (a_i, b_i) are relevant (e.g. are duplicates).
+
+MultipleNegativesRankingLoss takes a random subset of these, for example (a_1, b_1), ..., (a_n, b_n). a_i and b_i are considered to be relevant and should be close in vector space. All other b_j (for i != j) are negative examples and the distance between a_i and b_j should be maximized. Note: MultipleNegativesRankingLoss only works if a random b_j is likely not to be relevant for a_i. This is the case for our duplicate questions dataset: If a sample randomly b_j, it is unlikely to be a duplicate of a_i.
+
+
+The model we get works well for duplicate questions mining and for duplicate questions information retrieval. For question pair classification, other losses (like OnlineConstrativeLoss) work better.
 """
 
 import logging
@@ -20,6 +22,7 @@ from sentence_transformers import (
     SparseEncoderTrainer,
     SparseEncoderTrainingArguments,
 )
+from sentence_transformers.evaluation import SequentialEvaluator
 from sentence_transformers.sparse_encoder import evaluation, losses
 from sentence_transformers.training_args import BatchSamplers
 
@@ -39,15 +42,17 @@ def main():
         model_card_data=SparseEncoderModelCardData(
             language="en",
             license="apache-2.0",
-            model_name="splade-distilbert-base-uncased trained on GooAQ",
+            model_name="splade-distilbert-base-uncased trained on Quora Duplicates Questions",
         ),
     )
     model.max_seq_length = 256  # Set the max sequence length to 256 for the training
     print("Model max length:", model.max_seq_length)
 
-    # 2a. Load the GooAQ dataset: https://huggingface.co/datasets/sentence-transformers/gooaq
-    logging.info("Read the gooaq training dataset")
-    full_dataset = load_dataset("sentence-transformers/gooaq", split="train").select(range(100_000))
+    # 2a. Load the Quora Duplicate Questions dataset:  https://huggingface.co/datasets/sentence-transformers/quora-duplicates
+    logging.info("Read the Quora Duplicate Questions training dataset")
+    full_dataset = load_dataset("sentence-transformers/quora-duplicates", "triplet", split="train").select(
+        range(100000)
+    )
     dataset_dict = full_dataset.train_test_split(test_size=1_000, seed=12)
     train_dataset = dataset_dict["train"]
     eval_dataset = dataset_dict["test"]
@@ -65,14 +70,37 @@ def main():
         lambda_corpus=lambda_corpus,  # Weight for document loss
     )
 
-    # 4. Define evaluator. We use the SparseNanoBEIREvaluator, which is a light-weight evaluator
-    evaluator = evaluation.SparseNanoBEIREvaluator(
-        dataset_names=["msmarco", "nfcorpus", "nq"], show_progress_bar=True, batch_size=train_batch_size
+    # 4. Define an evaluators. We add 2 evaluators, that evaluate the model on Duplicate Questions pair classification,
+    # and NanoBeir with Duplicate Questions Information Retrieval
+    evaluators = []
+
+    duplicate_classes_dataset = load_dataset(
+        "sentence-transformers/quora-duplicates", "pair-class", split="train[-1000:]"
     )
+    binary_acc_evaluator = evaluation.SparseBinaryClassificationEvaluator(
+        sentences1=duplicate_classes_dataset["sentence1"],
+        sentences2=duplicate_classes_dataset["sentence2"],
+        labels=duplicate_classes_dataset["label"],
+        name="quora_duplicates_dev",
+        show_progress_bar=True,
+        similarity_fn_names=["cosine", "dot", "euclidean", "manhattan"],
+    )
+    evaluators.append(binary_acc_evaluator)
+
+    nano_beir_evaluator = evaluation.SparseNanoBEIREvaluator(
+        dataset_names=["msmarco", "nq", "nfcorpus", "quoraretrieval"],
+        show_progress_bar=True,
+        batch_size=train_batch_size,
+    )
+    evaluators.append(nano_beir_evaluator)
+
+    # Create a SequentialEvaluator. This SequentialEvaluator runs the 2 evaluators in a sequential order.
+    # We optimize the model with respect to the score from the last evaluator (scores[-1])
+    seq_evaluator = SequentialEvaluator(evaluators, main_score_function=lambda scores: scores[-1])
 
     # 5. Define the training arguments
     short_model_name = model_name if "/" not in model_name else model_name.split("/")[-1]
-    run_name = f"splade-{short_model_name}-gooaq"
+    run_name = f"splade-{short_model_name}-quora-duplicates"
     training_args = SparseEncoderTrainingArguments(
         # Required parameter:
         output_dir=f"models/{run_name}",
@@ -104,19 +132,15 @@ def main():
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
         loss=loss,
-        evaluator=evaluator,
+        evaluator=seq_evaluator,
     )
     trainer.train()
 
-    # 7. Evaluate the final model, using the complete NanoBEIR dataset
-    test_evaluator = evaluation.SparseNanoBEIREvaluator(show_progress_bar=True, batch_size=train_batch_size)
-    test_evaluator(model)
-
-    # 8. Save the final model
+    # 7. Save the final model
     final_output_dir = f"models/{run_name}/final"
     model.save_pretrained(final_output_dir)
 
-    # 9. (Optional) save the model to the Hugging Face Hub!
+    # 8. (Optional) save the model to the Hugging Face Hub!
     # It is recommended to run `huggingface-cli login` to log into your Hugging Face account first
     try:
         model.push_to_hub(run_name)
