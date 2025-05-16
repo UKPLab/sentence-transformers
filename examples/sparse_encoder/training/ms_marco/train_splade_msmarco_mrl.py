7043f8fdf7936a17eb5e7902390025ef8f1b9db2
arthurbr11 <arthurb242@icloud.com>
2025-05-16 14:46:02 +0200

    Update training examples and scripts for Sparse Encoder models; remove unused hard negative training script and enhance documentation.

--- examples/sparse_encoder/training/ms_marco/train_splade_msmarco_mrl.py
+++ examples/sparse_encoder/training/ms_marco/train_splade_msmarco_mrl.py
@@ -1,11 +1,9 @@
 """
 This scripts demonstrates how to train a Sparse Encoder model for Information Retrieval.
 
-As dataset, we use Quora Duplicates Questions, where we have pairs of duplicate questions.
+As dataset, we use sentence-transformers/msmarco-bm25, where we have triplets versions of MSMARCO mined thanks to BM25.
 
-As loss function, we use MultipleNegativesRankingLoss in the SpladeLoss. Here, we only need positive pairs, i.e., pairs of sentences/texts that are considered to be relevant. Our dataset looks like this (a_1, b_1), (a_2, b_2), ... with a_i / b_i a text and (a_i, b_i) are relevant (e.g. are duplicates).
-
-MultipleNegativesRankingLoss takes a random subset of these, for example (a_1, b_1), ..., (a_n, b_n). a_i and b_i are considered to be relevant and should be close in vector space. All other b_j (for i != j) are negative examples and the distance between a_i and b_j should be maximized. Note: MultipleNegativesRankingLoss only works if a random b_j is likely not to be relevant for a_i. This is the case for our duplicate questions dataset: If a sample randomly b_j, it is unlikely to be a duplicate of a_i.
+As loss function, we use MultipleNegativesRankingLoss in the SpladeLoss.
 
 """
 
@@ -20,9 +18,7 @@ from sentence_transformers import (
     SparseEncoderTrainer,
     SparseEncoderTrainingArguments,
 )
-from sentence_transformers.evaluation import SequentialEvaluator
 from sentence_transformers.sparse_encoder import evaluation, losses
-from sentence_transformers.training_args import BatchSamplers
 
 # Set the log level to INFO to get more information
 logging.basicConfig(format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
@@ -33,8 +29,11 @@ def main():
 
     train_batch_size = 12
     num_epochs = 1
+    lambda_query = 5e-5
+    lambda_corpus = 3e-5
+    learning_rate = 2e-5
 
-    # 1a. Load a model to finetune with 1b. (Optional) model card data
+    # 1. Define our SparseEncoder model
     model = SparseEncoder(
         model_name,
         model_card_data=SparseEncoderModelCardData(
@@ -46,8 +45,8 @@ def main():
     model.max_seq_length = 256  # Set the max sequence length to 256 for the training
     print("Model max length:", model.max_seq_length)
 
-    # 2a. Load the Quora Duplicate Questions dataset:  https://huggingface.co/datasets/sentence-transformers/quora-duplicates
-    logging.info("Read the Quora Duplicate Questions training dataset")
+    # 2. Load the MS MARCO dataset: https://huggingface.co/datasets/sentence-transformers/msmarco-bm25
+    logging.info("Read the MS MARCO training dataset")
     full_dataset = load_dataset("sentence-transformers/quora-duplicates", "triplet", split="train").select(
         range(100000)
     )
@@ -57,10 +56,7 @@ def main():
     logging.info(train_dataset)
     logging.info(eval_dataset)
 
-    # 3. Define our training loss.
-    lambda_query = 5e-5
-    lambda_corpus = 3e-5
-
+    # 3. Define our training loss
     loss = losses.SpladeLoss(
         model=model,
         loss=losses.SparseMultipleNegativesRankingLoss(model=model),
@@ -68,50 +64,26 @@ def main():
         lambda_corpus=lambda_corpus,  # Weight for document loss
     )
 
-    # 4. Define an evaluators. We add 2 evaluators, that evaluate the model on Duplicate Questions pair classification,
-    # and NanoBeir with Duplicate Questions Information Retrieval
-    evaluators = []
-
-    duplicate_classes_dataset = load_dataset(
-        "sentence-transformers/quora-duplicates", "pair-class", split="train[-1000:]"
+    # 4. Define the evaluator. We use the SparseNanoBEIREvaluator, which is a light-weight evaluator for English
+    evaluator = evaluation.SparseNanoBEIREvaluator(
+        dataset_names=["msmarco", "nfcorpus", "nq"], batch_size=train_batch_size
     )
-    binary_acc_evaluator = evaluation.SparseBinaryClassificationEvaluator(
-        sentences1=duplicate_classes_dataset["sentence1"],
-        sentences2=duplicate_classes_dataset["sentence2"],
-        labels=duplicate_classes_dataset["label"],
-        name="quora_duplicates_dev",
-        show_progress_bar=True,
-        similarity_fn_names=["cosine", "dot", "euclidean", "manhattan"],
-    )
-    evaluators.append(binary_acc_evaluator)
-
-    nano_beir_evaluator = evaluation.SparseNanoBEIREvaluator(
-        dataset_names=["msmarco", "nq", "nfcorpus", "quoraretrieval"],
-        show_progress_bar=True,
-        batch_size=train_batch_size,
-    )
-    evaluators.append(nano_beir_evaluator)
-
-    # Create a SequentialEvaluator. This SequentialEvaluator runs the 2 evaluators in a sequential order.
-    # We optimize the model with respect to the score from the last evaluator (scores[-1])
-    seq_evaluator = SequentialEvaluator(evaluators, main_score_function=lambda scores: scores[-1])
 
     # 5. Define the training arguments
     short_model_name = model_name if "/" not in model_name else model_name.split("/")[-1]
-    run_name = f"splade-{short_model_name}-quora-duplicates"
-    training_args = SparseEncoderTrainingArguments(
+    run_name = f"{short_model_name}-msmarco-mrl"
+    args = SparseEncoderTrainingArguments(
         # Required parameter:
         output_dir=f"models/{run_name}",
         # Optional training parameters:
         num_train_epochs=num_epochs,
         per_device_train_batch_size=train_batch_size,
         per_device_eval_batch_size=train_batch_size,
-        learning_rate=2e-5,
-        fp16=False,  # Set to False if you get an error that your GPU can't run on FP16
-        bf16=True,  # Set to True if you have a GPU that supports BF16
-        batch_sampler=BatchSamplers.NO_DUPLICATES,  # MultipleNegativesRankingLoss benefits from no duplicate samples in a batch
+        learning_rate=learning_rate,
         load_best_model_at_end=True,
         metric_for_best_model="eval_NanoBEIR_mean_dot_ndcg@10",
+        fp16=False,  # Set to False if you get an error that your GPU can't run on FP16
+        bf16=True,  # Set to True if you have a GPU that supports BF16
         # Optional tracking/debugging parameters:
         eval_strategy="steps",
         eval_steps=1650,
@@ -126,11 +98,11 @@ def main():
     # 6. Create the trainer & start training
     trainer = SparseEncoderTrainer(
         model=model,
-        args=training_args,
+        args=args,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
         loss=loss,
-        evaluator=seq_evaluator,
+        evaluator=evaluator,
     )
     trainer.train()
 
