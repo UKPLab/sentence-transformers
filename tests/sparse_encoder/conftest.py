c9e0586e1d924c30b34b7fd5fef54b315f3fc710
Tom Aarsen <Cubiegamedev@gmail.com>
2025-05-20 17:42:14 +0200

    Add test cases for model.decode(), currently failing

--- tests/sparse_encoder/conftest.py
+++ tests/sparse_encoder/conftest.py
@@ -10,6 +10,11 @@ def splade_bert_tiny_model() -> SparseEncoder:
     return SparseEncoder("sparse-encoder-testing/splade-bert-tiny-nq")
 
 
+@pytest.fixture(scope="session")
+def splade_bert_tiny_model_reused() -> SparseEncoder:
+    return SparseEncoder("sparse-encoder-testing/splade-bert-tiny-nq")
+
+
 @pytest.fixture()
 def csr_bert_tiny_model() -> SparseEncoder:
     return SparseEncoder("sentence-transformers-testing/stsb-bert-tiny-safetensors")
