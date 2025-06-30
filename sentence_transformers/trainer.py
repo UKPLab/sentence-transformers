3e7d2fa81995791f5b612e9f8e675a1a77b11b7b
Tom Aarsen <37621491+tomaarsen@users.noreply.github.com>
2025-06-30 14:06:26 +0200

    Partially revert 380660e9e400b273b3f086790517ec71ff8159f4, simplify trainer checkpoint loading (#49)

--- sentence_transformers/trainer.py
+++ sentence_transformers/trainer.py
@@ -1012,27 +1012,7 @@ class SentenceTransformerTrainer(Trainer):
     def _load_from_checkpoint(self, checkpoint_path: str) -> None:
         model_class = self.model.__class__
         loaded_model = model_class(checkpoint_path, trust_remote_code=self.model.trust_remote_code)
-
-        # Let's try to load PEFT Adapters if they are present in the checkpoint.
-        try:
-            checkpoint_adapters = loaded_model.active_adapters()
-        except (ValueError, TypeError):
-            # If not, we can assume that the checkpoint is a full model without adapters,
-            # and we can move over the state dict directly.
-            self.model.load_state_dict(loaded_model.state_dict())
-        else:
-            # If we can load adapters, we should load them one by one, potentially removing
-            # identically named adapters from the model being trained.
-            # But first, because we might have non-transformers weights, we also non-strictly load the state dict
-            self.model.load_state_dict(loaded_model.state_dict(), strict=False)
-            try:
-                train_adapters = self.model.active_adapters()
-            except ValueError:
-                train_adapters = []
-            for adapter_idx, adapter_name in enumerate(checkpoint_adapters):
-                if adapter_name in train_adapters:
-                    self.model.delete_adapter(adapter_name)
-                self.model.load_adapter(checkpoint_path, adapter_name=adapter_name, is_trainable=(adapter_idx == 0))
+        self.model.load_state_dict(loaded_model.state_dict())
 
     def _include_prompt_length(self) -> bool:
         """
